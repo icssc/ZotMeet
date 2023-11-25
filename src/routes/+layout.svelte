3222228467c5f13ce57e8e9c70c28a4378c12081
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-11-25 05:54:11 +0000

    feat: ✨ setup and theme

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "../app.pcss";
+</script>
+
+<slot />
+
+<!-- Global styles -->
+<style lang="postcss">
+  :global(html) {
+    font-family: sans-serif;
+    font-size: 16px;
+    line-height: 1.5;
+    color: #333;
+  }
+</style>
