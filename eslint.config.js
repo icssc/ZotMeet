31f3286650cdbb33f3802aa757b62e920ec9e549
Aponia <brianmv@uci.edu>
2023-11-25 20:53:30 -0800

    chore: 🔧 ran formatter

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
 // @ts-check
 
-import path from 'node:path'
-import url from 'node:url'
+import path from "node:path";
+import url from "node:url";
 
-import { FlatCompat } from '@eslint/eslintrc'
+import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = url.fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /**
  * Backwards compatibility module.
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: {},
-})
+});
 
 /**
  * @type{import('eslint').Linter.FlatConfig[]}
@@ -24,53 +24,53 @@ const compat = new FlatCompat({
 const config = [
   ...compat.config({
     root: true,
-    parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser",
     parserOptions: {
-      sourceType: 'module',
+      sourceType: "module",
       ecmaVersion: 2020,
-      extraFileExtensions: ['.svelte'],
+      extraFileExtensions: [".svelte"],
     },
     extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'standard-with-typescript',
-      'airbnb-base',
-      'airbnb-typescript/base',
-      'plugin:svelte/recommended',
-      'prettier',
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "standard-with-typescript",
+      "airbnb-base",
+      "airbnb-typescript/base",
+      "plugin:svelte/recommended",
+      "prettier",
     ],
-    plugins: ['import', '@typescript-eslint'],
+    plugins: ["import", "@typescript-eslint"],
     rules: {
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
         {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
         },
       ],
-      'import/first': 'error',
-      'import/newline-after-import': 'error',
-      'import/order': [
-        'error',
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/order": [
+        "error",
         {
-          alphabetize: { order: 'asc' },
-          'newlines-between': 'always',
+          alphabetize: { order: "asc" },
+          "newlines-between": "always",
         },
       ],
     },
     overrides: [
       {
-        files: ['*.svelte'],
-        parser: 'svelte-eslint-parser',
+        files: ["*.svelte"],
+        parser: "svelte-eslint-parser",
         parserOptions: {
-          parser: '@typescript-eslint/parser',
+          parser: "@typescript-eslint/parser",
         },
       },
     ],
-    ignorePatterns: ['.svelte-kit', '*.config.*'],
+    ignorePatterns: [".svelte-kit", "*.config.*"],
   }),
-]
+];
 
-export default config
+export default config;
