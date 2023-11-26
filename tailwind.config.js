f0847c46186ca62d839785aa75932ecf4e84d952
Aponia <brianmv@uci.edu>
2023-11-25 19:36:47 -0800

    style: 🎨 cleanup tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 // @ts-check
-import { join } from "path";
+
+import { join } from "node:path";
 
 import { skeleton } from "@skeletonlabs/tw-plugin";
 
 import { zotmeetTheme } from "./theme";
 
-/** @type {import('tailwindcss').Config} */
-export default {
-  // Opt for dark mode to be handled via the class method
+/**
+ * @type {import('tailwindcss').Config}
+ */
+const config = {
   darkMode: "class",
   content: [
     "./src/**/*.{html,js,svelte,ts}",
-    // Append the path to the Skeleton package
     join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte,ts}"),
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [
     skeleton({
       themes: { custom: [zotmeetTheme] },
     }),
   ],
 };
+
+export default config;
