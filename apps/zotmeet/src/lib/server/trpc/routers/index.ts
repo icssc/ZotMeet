618180d1fd6623b6b442fa0b9fb23907fcb6baf0
Aponia <brianmv@uci.edu>
2023-11-26 16:06:28 -0800

    feat: ✨ router for csl and science library data

--- apps/zotmeet/src/lib/server/trpc/routers/index.ts
+++ apps/zotmeet/src/lib/server/trpc/routers/index.ts
@@ -2,10 +2,17 @@ import { type } from 'arktype'
 
 import { router, procedure } from '../init'
 
+import { data as cslData } from '$lib/data/csl'
+import { data as scienceLibraryData } from '$lib/data/science-library'
+
 export const appRouter = router({
   hello: procedure.input(type({ text: 'string' }).assert).query(({ input }) => ({
     greeting: `hello ${input.text}`,
   })),
+
+  csl: procedure.query(() => cslData),
+
+  scienceLibrary: procedure.query(() => scienceLibraryData),
 })
 
 // export type definition of API
