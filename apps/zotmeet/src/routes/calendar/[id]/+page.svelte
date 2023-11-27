d53506feda76c353438e21897baee19dec56f63d
Aponia <brianmv@uci.edu>
2023-11-27 06:53:23 -0800

    feat: ✨ proof of concept calendar id route

---
+++ apps/zotmeet/src/routes/calendar/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { PageData } from '../$types'
+
+  export let data: PageData
+</script>
+
+<div>
+  <p class="text-4xl text-center font-semibold">
+    Reservation ID: {data.id}
+  </p>
+</div>
