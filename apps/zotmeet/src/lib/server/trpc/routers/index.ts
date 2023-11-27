060fcbbea17c0a00564bf0d9110426088c967aca
Aponia <brianmv@uci.edu>
2023-11-27 07:37:09 -0800

    refactor: ♻️ combine routers

--- apps/zotmeet/src/lib/server/trpc/routers/index.ts
+++ apps/zotmeet/src/lib/server/trpc/routers/index.ts
@@ -1,46 +1,10 @@
-import { type } from 'arktype'
-import dayjs from 'dayjs'
+import { router } from '../init'
 
-import { router, procedure } from '../init'
-
-import { data as cslData } from '$lib/data/csl'
-import { data as scienceLibraryData } from '$lib/data/science-library'
+import { cslRouter } from './csl'
 
 export const appRouter = router({
-  hello: procedure.input(type({ text: 'string' }).assert).query(({ input }) => ({
-    greeting: `Hello, ${input.text}~`,
-  })),
-
-  csl: procedure.query(() => cslData),
-
-  scienceLibrary: procedure.query(() => scienceLibraryData),
-
-  cslById: procedure.input(type('number | undefined').assert).query(({ input }) => {
-    if (input == null) {
-      return []
-    }
-
-    const rooms = cslData
-      .flatMap((slot) =>
-        slot.rooms.map((room) => ({
-          ...room,
-          start: dayjs(slot.segment_start_time, 'H:mm A'),
-        })),
-      )
-      .filter((room) => room.room_id === input)
-      .map((room) => ({
-        ...room,
-        start: room.start.toDate(),
-        end: room.start.add(30, 'minutes').toDate(),
-      }))
-
-    return rooms
-  }),
-
-  cslRoomIds: procedure.query(() =>
-    Array.from(new Set(cslData.flatMap((slot) => slot.rooms).map((room) => room.room_id))),
-  ),
+  csl: cslRouter,
 })
 
-// export type definition of API
+// export type definition of API.
 export type AppRouter = typeof appRouter
