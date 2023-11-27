16f1d671fb29584c826b60186662ea3a8a83777e
Aponia <brianmv@uci.edu>
2023-11-26 17:23:03 -0800

    style: 🎨 cleaned up hooks file

--- apps/zotmeet/src/hooks.server.ts
+++ apps/zotmeet/src/hooks.server.ts
@@ -1,18 +1,27 @@
 import { adapt } from '@aponia.js/adapter-prisma'
-import { Auth } from '@aponia.js/core'
-import { Session } from '@aponia.js/core/session'
+import { Auth, Session } from '@aponia.js/core'
 import { createAuthHelpers } from '@aponia.js/sveltekit'
 import GitHub from '@auth/core/providers/github'
 import Google from '@auth/core/providers/google'
 import { sequence } from '@sveltejs/kit/hooks'
 
+import { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private'
 import { prisma } from '$lib/server/db'
 
 const session = new Session()
 
 const auth = new Auth({
   session,
-  providers: [Google({}), GitHub({})],
+  providers: [
+    Google({
+      clientId: GOOGLE_ID,
+      clientSecret: GOOGLE_SECRET,
+    }),
+    GitHub({
+      clientId: GITHUB_ID,
+      clientSecret: GITHUB_SECRET,
+    }),
+  ],
 })
 
 adapt(auth, prisma)
