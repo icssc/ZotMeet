e7fbee0a6c4a82d63c0afea923bfebb2823e6750
Aponia <brianmv@uci.edu>
2023-11-27 07:36:17 -0800

    feat: ✨ csl router

---
+++ apps/zotmeet/src/lib/server/trpc/routers/csl.ts
@@ -0,0 +1,58 @@
+import { type } from 'arktype'
+import dayjs from 'dayjs'
+
+import { router, procedure } from '../init'
+
+import { data as cslData } from '$lib/data/csl'
+
+// Scuffed data pre-processing.
+
+const allCslRooms = cslData.flatMap((slot) => slot.rooms)
+
+const cslRooms = allCslRooms.reduce<Record<string, string>>((acc, room) => {
+  acc[room.room_id] = room.room_name
+  return acc
+}, {})
+
+const cslRoomIds = Object.keys(cslRooms)
+
+const cslRoomNames = Object.values(cslRooms)
+
+/**
+ * The CSL router handles requests for CSL data.
+ */
+export const cslRouter = router({
+  /**
+   * Returns all the rooms at CSL.
+   */
+  rooms: procedure.query(() => ({
+    rooms: cslRooms,
+    roomIds: cslRoomIds,
+    roomNames: cslRoomNames,
+  })),
+
+  /**
+   * Returns all the available timeslots for a specified room at CSL.
+   */
+  byId: procedure.input(type('number | undefined').assert).query(({ input }) => {
+    if (input == null) {
+      return []
+    }
+
+    const rooms = cslData
+      .flatMap((slot) =>
+        slot.rooms.map((room) => ({
+          ...room,
+          start: dayjs(slot.segment_start_time, 'H:mm A'),
+        })),
+      )
+      .filter((room) => room.room_id === input)
+      .map((room) => ({
+        ...room,
+        start: room.start.toDate(),
+        end: room.start.add(30, 'minutes').toDate(),
+      }))
+
+    return rooms
+  }),
+})
