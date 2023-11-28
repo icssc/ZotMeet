5ac906f0d43c3182aa4e6fe5626498b365d9a2ad
Aponia <brianmv@uci.edu>
2023-11-27 17:26:19 -0800

    fix: 🐛 try uploading prisma

--- apps/zotmeet/svelte.config.js
+++ apps/zotmeet/svelte.config.js
@@ -1,8 +1,30 @@
 // @ts-check
 
+import fs from 'node:fs'
+import path from 'node:path'
 import adapter from '@svelte.kit/adapter-aws'
 import { mdsvex } from 'mdsvex'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+
+/**
+ * Where all Prisma related files are located.
+ */
+const prismaClientDirectory = path.resolve('node_modules', 'prisma')
+
+/**
+ * Name of the schema file.
+ */
+const prismaSchemaFile = 'schema.prisma'
+
+/**
+ * Where the prisma schema is located.
+ */
+const prismaSchema = path.resolve('prisma', prismaSchemaFile)
+
+/**
+ * Name of the Prisma query engine file that's used on AWS Lambda.
+ */
+const prismaQueryEngineFile = 'libquery_engine-linux-arm64-openssl-1.0.x.so.node'
 
 /**
  * @type {import('@sveltejs/kit').Config}
@@ -16,7 +38,18 @@ const config = {
     }),
   ],
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      lambdaUpload: (directory) => {
+        fs.copyFileSync(
+          path.join(prismaClientDirectory, prismaQueryEngineFile),
+          path.join(directory, prismaQueryEngineFile),
+        )
+
+        fs.chmodSync(path.join(directory, prismaQueryEngineFile), 0o755)
+
+        fs.copyFileSync(prismaSchema, path.join(directory, prismaSchemaFile))
+      },
+    }),
     env: {
       dir: '../..',
     },
