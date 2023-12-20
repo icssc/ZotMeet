edb2a6032f05fee22daee310877430c3fc4ea1af
Kevin Wu <kevinwu098@gmail.com>
2023-12-20 04:51:32 -0800

    style: 🎨 slightly restyle groups

--- src/lib/components/summary/GroupList.svelte
+++ src/lib/components/summary/GroupList.svelte
@@ -5,14 +5,16 @@
 
 <div class="flex flex-col gap-4">
   <h1 class="text-4xl font-bold border-b border-surface-400-500-token">Groups</h1>
-  <div class="flex gap-2 overflow-x-auto md:gap-4 snap-x snap-mandatory scroll-smooth">
+  <div
+    class="flex gap-2 overflow-x-auto md:gap-4 snap-x snap-mandatory scroll-smooth hide-scrollbar"
+  >
     {#each groups as group (group.id)}
       <a href={group.link} target="_blank" referrerpolicy="no-referrer">
         <div
           class="flex h-24 p-3 bg-center bg-cover rounded-lg md:h-36 snap-start w-36 md:w-64 card"
-          style="background-image: url({group.img})"
+          style="background-image:url({group.img})"
         >
-          <p class="mt-auto font-bold text-white line-clamp-2 max-h-12">
+          <p class="mt-auto text-lg font-bold text-white md:text-xl line-clamp-2 max-h-12">
             {group.name}
           </p>
         </div>
