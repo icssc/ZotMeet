2f80f62a4527cdd62dd1d58e36d844447b24dbd7
Kevin Wu <kevinwu098@gmail.com>
2023-12-20 03:43:18 -0800

    chore: 🔧 add iconify icon set

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vitest/config";
+import Icons from "unplugin-icons/vite";
+import { defineProject } from "vitest/config";
 
-export default defineConfig({
-  plugins: [sveltekit()],
+export default defineProject({
+  plugins: [
+    sveltekit(),
+    Icons({
+      compiler: "svelte",
+    }),
+  ],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
   },
