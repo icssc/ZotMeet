4e43d7e360dc3f9a1eecba666394d93e652f88f0
Kevin Wu <kevinwu098@gmail.com>
2023-12-21 22:21:37 -0800

    style: 🎨 apply styling changes to unscheduled meetings

--- src/routes/summary/+page.svelte
+++ src/routes/summary/+page.svelte
@@ -293,45 +293,39 @@
             {/each}
           </div>
         {:else if tabSet === 1}
-          <div class="flex flex-col gap-4">
-            <div class="flex flex-col gap-2">
-              {#each unscheduledMeetings as meeting}
-                <a href={meeting.link} target="_blank" referrerpolicy="no-referrer">
-                  <div
-                    class="flex flex-col justify-between gap-4 p-3 bg-center bg-cover rounded-lg md:items-center h-fit md:flex-row card variant-ghost hover:variant-filled-surface"
-                  >
-                    <div class="flex flex-wrap items-center justify-between w-full gap-2">
-                      <div class="flex flex-col gap-y-1">
-                        <a href={meeting.link} target="_blank" referrerpolicy="no-referrer">
-                          <p class="text-xl font-bold md:text-2xl line-clamp-1 max-h-12">
-                            {meeting.name}
-                          </p>
-                        </a>
+          <div class="flex flex-col gap-2">
+            {#each unscheduledMeetings as meeting}
+              <div
+                class="flex flex-col justify-between gap-4 p-3 bg-center bg-cover rounded-lg md:items-center h-fit md:flex-row card hover:variant-ghost"
+              >
+                <div class="flex flex-wrap items-center justify-between w-full gap-2">
+                  <div class="flex flex-col gap-y-1">
+                    <a href={meeting.link} target="_blank" referrerpolicy="no-referrer">
+                      <p class="text-xl font-bold md:text-2xl line-clamp-1 max-h-12">
+                        {meeting.name}
+                      </p>
+                    </a>
 
-                        <div class="flex flex-row flex-wrap gap-x-4">
-                          <p class="flex items-center gap-1 text-md md:text-lg">
-                            <CalendarIcon />
-                            {convertIsoToDate(meeting.startDate)} - {convertIsoToDate(
-                              meeting.endDate,
-                            )}
-                          </p>
-                          <p class="flex items-center gap-1 text-md md:text-lg">
-                            <ClockIcon />
-                            {convertTo12HourFormat(meeting.startTime)} - {convertTo12HourFormat(
-                              meeting.endTime,
-                            )}
-                          </p>
-                          <p class="flex items-center gap-1 text-md md:text-lg">
-                            <LocationIcon />
-                            {meeting.location}
-                          </p>
-                        </div>
-                      </div>
+                    <div class="flex flex-row flex-wrap gap-x-4">
+                      <p class="flex items-center gap-1 text-md md:text-lg">
+                        <CalendarIcon />
+                        {convertIsoToDate(meeting.startDate)} - {convertIsoToDate(meeting.endDate)}
+                      </p>
+                      <p class="flex items-center gap-1 text-md md:text-lg">
+                        <ClockIcon />
+                        {convertTo12HourFormat(meeting.startTime)} - {convertTo12HourFormat(
+                          meeting.endTime,
+                        )}
+                      </p>
+                      <p class="flex items-center gap-1 text-md md:text-lg">
+                        <LocationIcon />
+                        {meeting.location}
+                      </p>
                     </div>
                   </div>
-                </a>
-              {/each}
-            </div>
+                </div>
+              </div>
+            {/each}
           </div>
         {/if}
       </svelte:fragment>
