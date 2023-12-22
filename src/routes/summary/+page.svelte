6a82de17d63781882c465b37fbac939b8bd577f8
Kevin Wu <kevinwu098@gmail.com>
2023-12-21 22:19:14 -0800

    feat: ✨ move meeting link to title

--- src/routes/summary/+page.svelte
+++ src/routes/summary/+page.svelte
@@ -236,57 +236,57 @@
 
                 <div class="flex flex-col gap-2">
                   {#each groupedMeetingsByDate[date] as meeting}
-                    <a href={meeting.link} target="_blank" referrerpolicy="no-referrer">
-                      <div
-                        class="flex flex-col justify-between gap-4 p-3 bg-center bg-cover rounded-lg md:items-center h-fit md:flex-row card hover:variant-ghost"
-                      >
-                        <div class="flex flex-wrap items-center justify-between w-full gap-2">
-                          <div class="flex flex-col gap-y-1">
+                    <div
+                      class="flex flex-col justify-between gap-4 p-3 bg-center bg-cover rounded-lg md:items-center h-fit md:flex-row card hover:variant-ghost"
+                    >
+                      <div class="flex flex-wrap items-center justify-between w-full gap-2">
+                        <div class="flex flex-col gap-y-1">
+                          <a href={meeting.link} target="_blank" referrerpolicy="no-referrer">
                             <p class="text-xl font-bold md:text-2xl line-clamp-1 max-h-12">
                               {meeting.name}
                             </p>
+                          </a>
 
-                            <div class="flex flex-row flex-wrap gap-x-4">
-                              <p class="flex items-center gap-1 text-md md:text-lg">
-                                <ClockIcon />
-                                {convertTo12HourFormat(meeting.startTime)} - {convertTo12HourFormat(
-                                  meeting.endTime,
-                                )}
-                              </p>
-                              <p class="flex items-center gap-1 text-md md:text-lg">
-                                <LocationIcon />
-                                {meeting.location}
-                              </p>
-                            </div>
+                          <div class="flex flex-row flex-wrap gap-x-4">
+                            <p class="flex items-center gap-1 text-md md:text-lg">
+                              <ClockIcon />
+                              {convertTo12HourFormat(meeting.startTime)} - {convertTo12HourFormat(
+                                meeting.endTime,
+                              )}
+                            </p>
+                            <p class="flex items-center gap-1 text-md md:text-lg">
+                              <LocationIcon />
+                              {meeting.location}
+                            </p>
                           </div>
                         </div>
-
-                        <RadioGroup
-                          class="flex items-center h-fit w-fit"
-                          active="variant-filled-primary"
-                          hover="hover:variant-soft-primary"
-                        >
-                          <RadioItem
-                            bind:group={meeting.attending}
-                            name="justify"
-                            value={"Yes"}
-                            required>Yes</RadioItem
-                          >
-                          <RadioItem
-                            bind:group={meeting.attending}
-                            name="justify"
-                            value={"No"}
-                            required>No</RadioItem
-                          >
-                          <RadioItem
-                            bind:group={meeting.attending}
-                            name="justify"
-                            value={"Maybe"}
-                            required>Maybe</RadioItem
-                          >
-                        </RadioGroup>
                       </div>
-                    </a>
+
+                      <RadioGroup
+                        class="flex items-center h-fit w-fit"
+                        active="variant-filled-primary"
+                        hover="hover:variant-soft-primary"
+                      >
+                        <RadioItem
+                          bind:group={meeting.attending}
+                          name="justify"
+                          value={"Yes"}
+                          required>Yes</RadioItem
+                        >
+                        <RadioItem
+                          bind:group={meeting.attending}
+                          name="justify"
+                          value={"No"}
+                          required>No</RadioItem
+                        >
+                        <RadioItem
+                          bind:group={meeting.attending}
+                          name="justify"
+                          value={"Maybe"}
+                          required>Maybe</RadioItem
+                        >
+                      </RadioGroup>
+                    </div>
                   {/each}
                 </div>
               </div>
@@ -302,9 +302,11 @@
                   >
                     <div class="flex flex-wrap items-center justify-between w-full gap-2">
                       <div class="flex flex-col gap-y-1">
-                        <p class="text-xl font-bold md:text-2xl line-clamp-1 max-h-12">
-                          {meeting.name}
-                        </p>
+                        <a href={meeting.link} target="_blank" referrerpolicy="no-referrer">
+                          <p class="text-xl font-bold md:text-2xl line-clamp-1 max-h-12">
+                            {meeting.name}
+                          </p>
+                        </a>
 
                         <div class="flex flex-row flex-wrap gap-x-4">
                           <p class="flex items-center gap-1 text-md md:text-lg">
