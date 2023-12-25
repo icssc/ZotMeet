2b8599ee6f0d4573213ea84c144a739e3e5bd719
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-12-25 20:59:37 +0000

    feat: ✨ add sidebar icons

--- src/lib/components/SideBar/SideBar.svelte
+++ src/lib/components/SideBar/SideBar.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import SideBarFooter from "./SideBarFooter.svelte";
   import SideBarLink from "./SideBarLink.svelte";
+
+  import CalendarIcon from "~icons/material-symbols/calendar-clock";
+  import SettingsIcon from "~icons/material-symbols/settings-rounded";
 </script>
 
 <div
@@ -15,8 +18,8 @@
   </div>
 
   <div id="sidebar-links" class="flex flex-col">
-    <SideBarLink icon="https://via.placeholder.com/50" label="Summary" href="/summary" />
-    <SideBarLink icon="https://via.placeholder.com/50" label="Settings" href="/settings" />
+    <SideBarLink Icon={CalendarIcon} label="Summary" href="/summary" />
+    <SideBarLink Icon={SettingsIcon} label="Settings" href="/settings" />
   </div>
 
   <div id="sidebar-footer-container" class="mt-auto m-2">
--- src/lib/components/SideBar/SideBarLink.svelte
+++ src/lib/components/SideBar/SideBarLink.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
-  export let icon: string;
+  /**
+   * Should be an SVG icon from Iconify `~icons/material-symbols`
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export let Icon: any;
   export let label: string;
   export let href: string;
 </script>
 
 <a id="sidebar-link-{label}" {href}>
-  <div class="flex flex-row whitespace-nowrap hover:bg-secondary-500 w-full gap-2 pl-5 p-3 rounded">
-    <img src={icon} alt="Icon" class="w-8 h-8" />
+  <div class="flex flex-row whitespace-nowrap hover:bg-secondary-500 w-full gap-4 pl-5 p-3 rounded">
+    <Icon class="w-8 h-8" />
     <span class="text-lg">{label}</span>
   </div>
 </a>
