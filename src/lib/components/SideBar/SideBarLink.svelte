2a8e18e461a3f947d06be6aa0aa58bf690981811
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-12-26 07:00:41 +0000

    fix: 🐛 sidebar link closes drawer

--- src/lib/components/SideBar/SideBarLink.svelte
+++ src/lib/components/SideBar/SideBarLink.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { getDrawerStore } from "@skeletonlabs/skeleton";
+
   /**
    * Should be an SVG icon from Iconify `~icons/material-symbols`
    */
@@ -6,9 +8,11 @@
   export let Icon: any;
   export let label: string;
   export let href: string;
+
+  const closeDrawer = getDrawerStore().close;
 </script>
 
-<a id="sidebar-link-{label}" {href}>
+<a id="sidebar-link-{label}" {href} on:click={closeDrawer}>
   <div
     class="flex flex-row whitespace-nowrap hover:bg-secondary-500 w-full gap-4 p-5 lg:p-3 rounded-lg"
   >
