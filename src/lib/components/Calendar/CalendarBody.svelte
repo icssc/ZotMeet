df4d9034492e002e66a4f2d528b5d5869dc64d71
Sean Fong <seancfong@gmail.com>
2024-01-14 22:40:50 -0800

    feat: ✨ create calendar/meeting setup page (#10)
    * feat: ✨ create calendar component
    
    * feat: ✨ add dynamically populating days for calendar
    
    * feat: ✨ add back and forth selection between consecutive calendar months
    
    * chore: 🔧 move calendar utils to utils directory
    
    * feat: ✨ add mobile drag multiselect on calendar
    
    * feat: ✨ added calendar day selection on desktop and mobile
    
    * fix: 🐛 prevent unwanted cancel events from scrolling
    
    * chore: 🔧 refactored functions from calendar utils as static methods of day class
    
    * refactor: ♻️ moved date range selection function to selection store file
    
    * refactor: ♻️ improve readability of reverse selection state variables
    
    * refactor: ♻️ added docstrings to event handlers
    
    * refactor: ♻️ improve code readability of calendar generator function
    
    * fix: 🐛 removed unintended behavior of over unselecting a date range
    
    * feat: ✨ extract chronological types to type file
    
    * fix: 🐛 extracted date comparison logic to a method to enable re-renders on multiselect
    
    * refactor: ♻️ rename CalendarDay class to Day
    
    * refactor: ♻️ rename calendar helper method and variables for readability
    
    ---------
    
    Co-authored-by: Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>

---
+++ src/lib/components/Calendar/CalendarBody.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+  import CalendarBodyDay from "$lib/components/Calendar/CalendarBodyDay.svelte";
+  import { Day } from "$lib/components/Calendar/CalendarDay";
+  import { updateSelectedRange } from "$lib/stores/calendarStores";
+
+  export let calendarDays: Day[][];
+  export let updateCalendar: () => void = () => {};
+
+  let startDaySelection: Day | null = null;
+  let endDaySelection: Day | null = null;
+
+  /**
+   * Updates the current highlight selection whenever a mobile user drags on the calendar
+   * @param e a TouchEvent object from a mobile user
+   */
+  const handleTouchMove = (e: TouchEvent): void => {
+    const touchingElement: Element | null = document.elementFromPoint(
+      e.touches[0].clientX,
+      e.touches[0].clientY,
+    );
+
+    if (!touchingElement) return;
+
+    const touchingDay = touchingElement.getAttribute("data-day");
+
+    if (startDaySelection && touchingDay) {
+      endDaySelection = Day.extractDayFromElement(touchingElement);
+    }
+  };
+
+  /**
+   * Creates the selectionupdateSelectedRange */
+  const handleEndSelection = (): void => {
+    if (startDaySelection && endDaySelection) {
+      try {
+        updateSelectedRange(startDaySelection, endDaySelection);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    updateCalendar();
+
+    startDaySelection = null;
+    endDaySelection = null;
+  };
+</script>
+
+<tbody>
+  {#each calendarDays as calendarWeek}
+    <tr>
+      {#each calendarWeek as calendarDay}
+        <td
+          class="py-3"
+          on:mouseup={() => {
+            if (startDaySelection) {
+              endDaySelection = calendarDay;
+              handleEndSelection();
+            }
+          }}
+        >
+          {#if calendarDay.day > 0}
+            {@const isHighlighted =
+              startDaySelection &&
+              endDaySelection &&
+              calendarDay.determineDayWithinBounds(startDaySelection, endDaySelection)}
+            <button
+              on:touchstart={(e) => {
+                if (e.cancelable) {
+                  e.preventDefault();
+                }
+                startDaySelection = calendarDay;
+              }}
+              on:mousedown={() => {
+                startDaySelection = calendarDay;
+              }}
+              on:touchmove={handleTouchMove}
+              on:mousemove={() => {
+                if (startDaySelection) {
+                  endDaySelection = calendarDay;
+                }
+              }}
+              on:touchend={(e) => {
+                if (e.cancelable) {
+                  e.preventDefault();
+                }
+                if (!endDaySelection) {
+                  endDaySelection = calendarDay;
+                }
+                handleEndSelection();
+              }}
+              tabindex="0"
+              class="relative flex justify-center w-full cursor-pointer select-none"
+            >
+              <CalendarBodyDay {isHighlighted} {calendarDay} />
+            </button>
+          {:else}
+            <div class="select-none">
+              <p class="p-2">&nbsp;</p>
+            </div>
+          {/if}
+        </td>
+      {/each}
+    </tr>
+  {/each}
+</tbody>
