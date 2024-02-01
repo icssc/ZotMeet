9660d1edaae1752a1373c6ed2c500b5d1371820c
Kevin Wu <kevinwu098@gmail.com>
2024-02-01 02:29:29 -0800

    feat: ✨ scaffold summary page

--- src/routes/summary/+page.svelte
+++ src/routes/summary/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
   import { TabGroup, Tab } from "@skeletonlabs/skeleton";
 
   import GroupList from "$lib/components/summary/GroupsCarousel.svelte";
@@ -34,5 +34,48 @@
         {/if}
       </svelte:fragment>
     </TabGroup>
+  </div>
+</div> -->
+
+<script lang="ts">
+  // import * as Carousel from "$lib/components/ui/carousel/index";
+  import GroupsCarousel from "$lib/components/summary/GroupsCarousel.svelte";
+  import ScheduledMeetingsList from "$lib/components/summary/ScheduledMeetingsList.svelte";
+  import * as Tabs from "$lib/components/ui/tabs";
+
+  // import UnscheduledMeetingsList from "$lib/components/summary/UnscheduledMeetingsList.svelte";
+</script>
+
+<div class="flex flex-col gap-8 px-4 pt-8 md:px-32">
+  <h1 class="text-4xl font-bold">Groups</h1>
+
+  <GroupsCarousel />
+
+  <!-- <Carousel.Root>
+    <Carousel.Content>
+      <Carousel.Item>Dog 1</Carousel.Item>
+      <Carousel.Item>Dog 2</Carousel.Item>
+      <Carousel.Item>Dog 3</Carousel.Item>
+    </Carousel.Content>
+    <Carousel.Previous />
+    <Carousel.Next />
+  </Carousel.Root> -->
+
+  <h1 class="text-4xl font-bold">Meetings</h1>
+
+  <div class="flex-center">
+    <Tabs.Root value="scheduled" class="flex-center w-full flex-col">
+      <Tabs.List>
+        <Tabs.Trigger value="scheduled">Scheduled</Tabs.Trigger>
+        <Tabs.Trigger value="unscheduled">Unscheduled</Tabs.Trigger>
+      </Tabs.List>
+
+      <div class="flex w-full flex-1 justify-start">
+        <Tabs.Content value="scheduled" class="w-full">
+          <ScheduledMeetingsList />
+        </Tabs.Content>
+        <Tabs.Content value="unscheduled">Change your password here.</Tabs.Content>
+      </div>
+    </Tabs.Root>
   </div>
 </div>
