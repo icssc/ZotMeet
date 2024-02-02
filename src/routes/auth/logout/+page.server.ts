455d15350f4103b8d3f8b0cf1fb12da5d1f31e72
Kevin Wu <kevinwu098@gmail.com>
2024-02-01 19:39:14 -0800

    refactor: clean up auth code (#28)
    * fix: 🐛 cleanup prisma schema
    
    * fix: 🐛 cleanup server hooks
    
    * fix: 🐛 cleanup login and register
    
    * refactor: ♻️ extract default function
    
    * refactor: ♻️ move code around

--- src/routes/auth/logout/+page.server.ts
+++ src/routes/auth/logout/+page.server.ts
@@ -3,13 +3,19 @@ import { redirect } from "@sveltejs/kit";
 import { auth } from "$lib/server/lucia";
 
 export const actions = {
-  default: async ({ locals }) => {
-    const session = await locals.auth.validate();
-    if (!session) {
-      throw redirect(302, "/auth");
-    }
-    await auth.invalidateSession(session.sessionId); // invalidate session
-    locals.auth.setSession(null); // remove cookie
-    throw redirect(302, "/auth");
-  },
+  default: logout,
 };
+
+interface LogoutInterface {
+  locals: App.Locals;
+}
+
+async function logout({ locals }: LogoutInterface) {
+  const session = await locals.auth.validate();
+  if (!session) {
+    throw redirect(302, "/auth");
+  }
+  await auth.invalidateSession(session.sessionId); // Invalidate session
+  locals.auth.setSession(null); // Remove cookie
+  throw redirect(302, "/auth"); // Redirect to auth page
+}
