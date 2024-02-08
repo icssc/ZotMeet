aa12aabde64a257d685125c58db2b6cfa3aa723d
Kevin Wu <kevinwu098@gmail.com>
2024-02-08 10:34:30 -0800

    feat: ✨ create, delete groups

--- src/routes/groups/+page.server.ts
+++ src/routes/groups/+page.server.ts
@@ -7,12 +7,16 @@ import { prisma } from "$lib/server/prisma";
 export const load: PageServerLoad = async (event) => {
   const user = event.locals.user;
 
+  if (!user) {
+    throw redirect(302, "/auth");
+  }
+
   return {
     groups: await prisma.groups.findMany({
       where: {
         members: {
           some: {
-            userId: user.userId,
+            userId: user.userId, // TODO: There probably should be an "owner" of a group
           },
         },
       },
@@ -22,18 +26,19 @@ export const load: PageServerLoad = async (event) => {
 
 export const actions: Actions = {
   createGroup: createGroup,
+  deleteGroup: deleteGroup,
 };
 
 async function createGroup({ request, locals }: { request: Request; locals: App.Locals }) {
   const session = await locals.auth.validate();
   if (!session) {
-    throw redirect(302, "/");
+    throw redirect(302, "/auth");
   }
 
-  const { name, description } = Object.fromEntries(await request.formData()) as Record<
-    string,
-    string
-  >;
+  const { name, description } = Object.fromEntries(await request.formData()) as {
+    name: string;
+    description: string;
+  };
 
   try {
     await prisma.groups.create({
@@ -50,6 +55,33 @@ async function createGroup({ request, locals }: { request: Request; locals: App.
   } catch (err) {
     console.error(err);
     return fail(500, { message: "Could not create group." });
+  }
+
+  return {
+    status: 201,
+  };
+}
+
+async function deleteGroup({ url, locals }: { url: URL; locals: App.Locals }) {
+  const session = await locals.auth.validate();
+  if (!session) {
+    throw redirect(302, "/");
+  }
+
+  const id = url.searchParams.get("id");
+  if (!id) {
+    return fail(400, { message: "Invalid request, could not delete group" });
+  }
+
+  try {
+    await prisma.groups.delete({
+      where: {
+        id: id,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return fail(500, { message: "Could not delete group." });
   }
 
   return {
---
+++ src/routes/groups/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+
+  $: ({ groups } = data);
+</script>
+
+<main>
+  <div>
+    <form action="?/createGroup" method="POST">
+      <div>Add Group</div>
+
+      <input type="text" name="name" id="name" placeholder="name" />
+      <input type="text" name="description" id="description" placeholder="description" />
+
+      <button type="submit">Submit</button>
+    </form>
+
+    <h2>Your groups:</h2>
+    {#each groups as group}
+      <div>
+        <header>name: {group.name}</header>
+        <p>
+          description: {group.description}
+        </p>
+        <form action="?/deleteGroup&id={group.id}" method="POST">
+          <button type="submit" class="secondary outline">Delete Group</button>
+        </form>
+      </div>
+    {/each}
+  </div>
+</main>
