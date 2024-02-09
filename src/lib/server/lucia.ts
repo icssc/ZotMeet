9378fe3a26e3c3b778d50c3673b68eab835bfe67
Kevin Wu <kevinwu098@gmail.com>
2024-02-09 10:55:43 -0800

    feat: ✨ disable google auth

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -1,14 +1,14 @@
 import { prisma } from "@lucia-auth/adapter-prisma";
-import { google } from "@lucia-auth/oauth/providers";
+// import { google } from "@lucia-auth/oauth/providers";
 import { lucia } from "lucia";
 import { sveltekit } from "lucia/middleware";
 
 import { dev } from "$app/environment";
-import {
-  GOOGLE_OAUTH_CLIENT_ID,
-  GOOGLE_OAUTH_CLIENT_SECRET,
-  GOOGLE_OAUTH_REDIRECT_URI,
-} from "$env/static/private";
+// import {
+//   GOOGLE_OAUTH_CLIENT_ID,
+//   GOOGLE_OAUTH_CLIENT_SECRET,
+//   GOOGLE_OAUTH_REDIRECT_URI,
+// } from "$env/static/private";
 import { prisma as client } from "$lib/server/prisma";
 
 export const auth = lucia({
@@ -36,14 +36,14 @@ export const auth = lucia({
   },
 });
 
-export const googleAuth = google(auth, {
-  clientId: GOOGLE_OAUTH_CLIENT_ID!,
-  clientSecret: GOOGLE_OAUTH_CLIENT_SECRET!,
-  redirectUri: GOOGLE_OAUTH_REDIRECT_URI!,
-  scope: [
-    "https://www.googleapis.com/auth/userinfo.profile",
-    "https://www.googleapis.com/auth/userinfo.email",
-  ],
-});
+// export const googleAuth = google(auth, {
+//   clientId: GOOGLE_OAUTH_CLIENT_ID!,
+//   clientSecret: GOOGLE_OAUTH_CLIENT_SECRET!,
+//   redirectUri: GOOGLE_OAUTH_REDIRECT_URI!,
+//   scope: [
+//     "https://www.googleapis.com/auth/userinfo.profile",
+//     "https://www.googleapis.com/auth/userinfo.email",
+//   ],
+// });
 
 export type Auth = typeof auth;
