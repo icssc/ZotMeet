e862a01e94175c688496736ea4bc501beecc45cb
Kevin Wu <kevinwu098@gmail.com>
2024-02-12 14:50:04 -0800

    refactor: ♻️ extract sidebar to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
+  import SideBar from "$lib/components/SidebarV2/SideBar.svelte";
   import "../app.pcss";
 </script>
 
-<slot />
+<div
+  class="flex h-full min-h-screen w-full flex-row bg-gradient-to-l from-[#EEEEEE] to-[#EAEFF2] pl-6 pt-6"
+>
+  <SideBar />
+  <slot />
+</div>
 
 <!-- Global styles -->
 <style lang="postcss">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,14 @@
 <script lang="ts">
   import Calendar from "$lib/components/CalendarV2/Calendar.svelte";
-  import SideBar from "$lib/components/SidebarV2/SideBar.svelte";
 </script>
 
-<main
-  class="flex h-full min-h-screen w-full flex-row bg-gradient-to-l from-[#EEEEEE] to-[#EAEFF2] pl-6 pt-6"
->
-  <SideBar />
-
-  <div class="flex min-h-full w-full flex-col gap-y-6 rounded-tl-xl border-2 bg-white px-4 pb-6">
-    <div class="pt-6">
-      <h1 class="font-montserrat text-2xl font-medium">Let's start planning your next meeting.</h1>
-      <h2 class="text-base text-gray-medium">
-        Select any dates and a time range that could work for you and your team.
-      </h2>
-    </div>
-
-    <Calendar />
+<main class="flex min-h-full w-full flex-col gap-y-6 rounded-tl-xl border-2 bg-white px-4 pb-6">
+  <div class="pt-6">
+    <h1 class="font-montserrat text-2xl font-medium">Let's start planning your next meeting.</h1>
+    <h2 class="text-base text-gray-medium">
+      Select any dates and a time range that could work for you and your team.
+    </h2>
   </div>
+
+  <Calendar />
 </main>
