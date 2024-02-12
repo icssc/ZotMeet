d65ec82d1a0b16ee61a65423b434cd852ec12cae
Kevin Wu <kevinwu098@gmail.com>
2024-02-12 13:36:59 -0800

    fix: 🐛 revert original calendarbodyday

--- src/lib/components/meetingSetup/Calendar/CalendarBodyDay.svelte
+++ src/lib/components/meetingSetup/Calendar/CalendarBodyDay.svelte
@@ -7,16 +7,16 @@
 
 {#if isHighlighted}
   <div
-    class="absolute left-1/2 top-1/2 h-10 w-10 -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary"
-  />
+    class="bg-primary-200 absolute left-1/2 top-1/2 h-10 w-10 -translate-x-1/2 -translate-y-1/2 rounded-full"
+  ></div>
 {:else if calendarDay.isSelected}
   <div
-    class="absolute left-1/2 top-1/2 h-10 w-10 -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary"
-  />
+    class="bg-primary-400 absolute left-1/2 top-1/2 h-10 w-10 -translate-x-1/2 -translate-y-1/2 rounded-full"
+  ></div>
 {/if}
 
 <p
-  class="relative text-base font-medium text-gray-500"
+  class="relative p-2 text-base font-medium text-gray-500"
   data-day={calendarDay.getDay()}
   data-month={calendarDay.getMonth()}
   data-year={calendarDay.getYear()}
