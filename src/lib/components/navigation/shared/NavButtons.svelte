c2deabc0ae95f827e81d72d9aebdd7f1497ff19c
Kevin Wu <kevinwu098@gmail.com>
2024-02-13 19:49:00 -0800

    style: 🎨 update icons

--- src/lib/components/navigation/shared/NavButtons.svelte
+++ src/lib/components/navigation/shared/NavButtons.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import AddBoxOutlineRounded from "~icons/material-symbols/add-box-outline-rounded";
-  import CalendarIcon from "~icons/material-symbols/calendar-clock";
-  import SettingsOutline from "~icons/material-symbols/settings-outline";
+  import AddBoxOutlineIcon from "~icons/material-symbols/add-box-outline";
+  import CalendarSearchOutlineIcon from "~icons/mdi/calendar-search-outline";
+  import SettingsOutline from "~icons/mdi/settings-outline";
 </script>
 
 <div class="flex flex-col gap-y-2 border-t-2 px-6 pt-6 text-gray-600 sm:px-8 md:border-0 md:p-0">
@@ -9,7 +9,7 @@
     <button
       class="btn h-12 w-full justify-start space-x-3 border-none bg-transparent text-xl font-normal shadow-none md:text-lg"
     >
-      <AddBoxOutlineRounded class="h-7 w-7" />
+      <AddBoxOutlineIcon class="h-7 w-7" />
       <p>New Meeting</p>
     </button>
   </a>
@@ -17,7 +17,7 @@
     <button
       class="btn h-12 w-full justify-start space-x-3 border-none bg-transparent text-xl font-normal shadow-none md:text-lg"
     >
-      <CalendarIcon class="h-7 w-7" />
+      <CalendarSearchOutlineIcon class="h-7 w-7" />
       <p>Summary</p>
     </button>
   </a>
