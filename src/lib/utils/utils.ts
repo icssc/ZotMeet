244a7922c0e40fefc6e0a2be0a27e4245b7ad1a4
Kevin Wu <kevinwu098@gmail.com>
2024-02-14 01:46:42 -0800

    docs: 📚️ document cn util

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Utility to generate dynamic class names for components.
+ * Combines class names provided as arguments using clsx utility
+ * Merges any Tailwind CSS class names using twMerge utility
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
