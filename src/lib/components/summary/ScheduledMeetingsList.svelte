448eb340b42a595595bfbb37d19eaefb31d415cb
Sean Fong <seancfong@gmail.com>
2024-02-22 03:14:04 -0800

    feat: ✨ add timeline to scheduled meetings

--- src/lib/components/summary/ScheduledMeetingsList.svelte
+++ src/lib/components/summary/ScheduledMeetingsList.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-  import { RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
-
   import { scheduledMeetings } from "$lib/stores/summaryStores";
   import {
-    convertIsoToWeekdayDate,
+    convertIsoToDate,
     convertTo12HourFormat,
+    getWeekdayFromIso,
     groupAndSortScheduledMeetings,
   } from "$lib/utils/summary-helpers";
   import LocationIcon from "~icons/material-symbols/location-on";
@@ -13,57 +12,55 @@
   const sortedMeetings = groupAndSortScheduledMeetings($scheduledMeetings);
 </script>
 
-{#each Object.keys(sortedMeetings) as date}
-  <div class="card variant-glass p-2">
-    <h2 class="mb-2 text-xl font-bold md:text-2xl">{convertIsoToWeekdayDate(date)}</h2>
+<ul class="timeline timeline-vertical">
+  {#each Object.entries(sortedMeetings) as [date, meetings] (date)}
+    <li class="[--timeline-col-start:4rem] [--timeline-row-start:auto]">
+      <hr class="!row-start-2 !w-0.5 bg-slate-300" />
+      <div class="timeline-start !row-start-1 !row-end-3 m-0 pr-2">
+        <p class="text-center text-xs font-bold uppercase text-gray-500">
+          {getWeekdayFromIso(date)}
+        </p>
+        <p class="text-center text-gray-400">{convertIsoToDate(date)}</p>
+      </div>
+      <div class="timeline-middle !row-start-1 !row-end-2 pt-1">
+        <div class="h-2 w-2 rounded-full bg-gray-400" />
+      </div>
+      <div class="timeline-end !row-start-2">
+        <div class="card p-2">
+          <div class="flex flex-col gap-2">
+            {#each meetings as meeting}
+              <div
+                class="hover:variant-ghost card flex h-fit flex-col justify-between gap-4 rounded-lg bg-cover bg-center p-3 md:flex-row md:items-center"
+              >
+                <div class="flex w-full flex-wrap items-center justify-between gap-2">
+                  <div class="flex flex-col gap-y-1">
+                    <a href={meeting.link} target="_blank" referrerpolicy="no-referrer">
+                      <p class="line-clamp-1 max-h-12 text-xl font-bold md:text-2xl">
+                        {meeting.name}
+                      </p>
+                    </a>
 
-    <div class="flex flex-col gap-2">
-      {#each sortedMeetings[date] as meeting}
-        <div
-          class="card flex h-fit flex-col justify-between gap-4 rounded-lg bg-cover bg-center p-3 hover:variant-ghost md:flex-row md:items-center"
-        >
-          <div class="flex w-full flex-wrap items-center justify-between gap-2">
-            <div class="flex flex-col gap-y-1">
-              <a href={meeting.link} target="_blank" referrerpolicy="no-referrer">
-                <p class="line-clamp-1 max-h-12 text-xl font-bold md:text-2xl">
-                  {meeting.name}
-                </p>
-              </a>
-
-              <div class="flex flex-row flex-wrap gap-x-4">
-                <p class="text-md flex items-center gap-1 md:text-lg">
-                  <ClockIcon />
-                  {convertTo12HourFormat(meeting.startTime)} - {convertTo12HourFormat(
-                    meeting.endTime,
-                  )}
-                </p>
-                <p class="text-md flex items-center gap-1 md:text-lg">
-                  <LocationIcon />
-                  {meeting.location}
-                </p>
+                    <div class="flex flex-row flex-wrap gap-x-4">
+                      <p class="text-md flex items-center gap-1 md:text-lg">
+                        <ClockIcon />
+                        {convertTo12HourFormat(meeting.startTime)} - {convertTo12HourFormat(
+                          meeting.endTime,
+                        )}
+                      </p>
+                      <p class="text-md flex items-center gap-1 md:text-lg">
+                        <LocationIcon />
+                        {meeting.location}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="flex w-full justify-center md:w-fit">yes no maybe</div>
               </div>
-            </div>
-          </div>
-
-          <div class="flex w-full justify-center md:w-fit">
-            <RadioGroup
-              class="flex h-fit w-fit items-center"
-              active="variant-filled-primary"
-              hover="hover:variant-soft-primary"
-            >
-              <RadioItem bind:group={meeting.attending} name="justify" value={"Yes"} required
-                >Yes</RadioItem
-              >
-              <RadioItem bind:group={meeting.attending} name="justify" value={"No"} required
-                >No</RadioItem
-              >
-              <RadioItem bind:group={meeting.attending} name="justify" value={"Maybe"} required
-                >Maybe</RadioItem
-              >
-            </RadioGroup>
+            {/each}
           </div>
         </div>
-      {/each}
-    </div>
-  </div>
-{/each}
+      </div>
+      <hr class="!row-start-3 !w-0.5 bg-slate-300" />
+    </li>
+  {/each}
+</ul>
--- src/lib/utils/summary-helpers.ts
+++ src/lib/utils/summary-helpers.ts
@@ -1,24 +1,14 @@
+import { WEEKDAYS } from "$lib/types/chrono";
 import type { Meeting, ScheduledMeeting, UnscheduledMeeting } from "$lib/types/meetings";
 
 export function convertIsoToDate(isoDateString: string): string {
-  const options: Intl.DateTimeFormatOptions = {
-    month: "short",
-    day: "numeric",
-  };
-
   const date = new Date(isoDateString);
-  return date.toLocaleDateString("en-US", { ...options, timeZone: "UTC" });
+  return `${date.getMonth() + 1}/${date.getDate()}`;
 }
 
-export function convertIsoToWeekdayDate(isoDateString: string): string {
-  const options: Intl.DateTimeFormatOptions = {
-    weekday: "short",
-    month: "short",
-    day: "numeric",
-  };
-
+export function getWeekdayFromIso(isoDateString: string): string {
   const date = new Date(isoDateString);
-  return date.toLocaleDateString("en-US", { ...options, timeZone: "UTC" });
+  return WEEKDAYS[date.getDay()];
 }
 
 export function convertTo12HourFormat(time: string): string {
