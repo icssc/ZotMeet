bd8c398163ede4a13f0555c4db8ec98c27004c12
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2024-02-22 17:40:03 -0800

    feat: ✨ set up backbone for drizzle migration

---
+++ src/lib/db/postgres.ts
@@ -0,0 +1,11 @@
+import postgres from "postgres";
+
+import { DATABASE_URL } from "$env/static/private";
+
+/**
+ * The PostgreSQL client. Lucia v2 does not have a Drizzle connector (v3 does),
+ * so you will have to use this instead of the Drizzle client.
+ *
+ * In all other cases, you should probably not use this unless you know what you are doing.
+ */
+export const client = postgres(DATABASE_URL, { max: 1, ssl: { rejectUnauthorized: false } });
---
+++ src/lib/db/schema.ts
@@ -0,0 +1,4 @@
+// Define your Drizzle schema here. Make sure to `export` every table/enum you declare.
+
+// Remove this line once you've declared at least one database object.
+export {};
