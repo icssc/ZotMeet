5a79024b619e97799b6bec087f715b69ee4bae2c
Sean Fong <seancfong@gmail.com>
2024-02-27 03:09:29 -0800

    fix: 🐛 remove persisting shadow visual bug on highlight

--- src/lib/components/creation/CalendarV2/CalendarBodyDay.svelte
+++ src/lib/components/creation/CalendarV2/CalendarBodyDay.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
   import type { ZotDate } from "$lib/utils/ZotDate";
-  import { cn } from "$lib/utils/utils";
 
   export let isHighlighted: boolean | null;
   export let calendarDay: ZotDate;
 </script>
 
 <p
-  class={cn(
-    "flex-center relative aspect-square h-8 w-8 text-base font-medium text-gray-dark md:h-12 md:w-12 md:text-xl",
-    calendarDay.isSelected &&
-      "rounded-lg bg-primary text-gray-light md:rounded-xl md:drop-shadow-[0_0px_16px_rgba(55,124,251,0.4)]",
-    isHighlighted && "rounded-lg bg-slate-base text-gray-dark drop-shadow-none md:rounded-xl",
-  )}
+  class="flex-center relative aspect-square h-8 w-8 text-base font-medium text-gray-dark md:h-12 md:w-12 md:text-xl"
+  class:day-selected={calendarDay.isSelected}
+  class:day-highlighted={isHighlighted}
   data-day={calendarDay.getDay()}
   data-month={calendarDay.getMonth() + 1}
   data-year={calendarDay.getYear()}
@@ -20,3 +16,13 @@
 >
   {calendarDay.getDay()}
 </p>
+
+<style lang="postcss">
+  .day-selected {
+    @apply rounded-lg bg-primary text-gray-light md:rounded-xl md:drop-shadow-[0_0px_16px_rgba(55,124,251,0.4)];
+  }
+
+  .day-highlighted {
+    @apply rounded-lg bg-slate-base text-gray-dark drop-shadow-none md:rounded-xl;
+  }
+</style>
