cfec577f2599faaab8ec89495b0f5cbcb6fab681
Kevin Wu <kevinwu098@gmail.com>
2024-03-07 16:39:28 -0800

    style: 🎨 make elements more visible w/ bg color

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -10,7 +10,7 @@
   let tabSet: string = "signIn";
 </script>
 
-<div class="mt-48 flex min-h-[100vh] justify-center">
+<div class="flex grow justify-center bg-blue-200 pt-48">
   <div class="mx-2 w-[450px] md:mx-16">
     {#if data.user}
       <p>{data.user.username} {data.user.userId}</p>
