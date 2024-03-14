998f99855201811b174df18fba5a109acd005867
Kevin Wu <kevinwu098@gmail.com>
2024-03-13 20:38:44 -0700

    feat: ✨ update logout

--- src/routes/auth/logout/+page.server.ts
+++ src/routes/auth/logout/+page.server.ts
@@ -1,21 +1,13 @@
-import { redirect } from "@sveltejs/kit";
+import { redirect, type Cookies } from "@sveltejs/kit";
 
-import { auth } from "$lib/server/lucia";
+import { deleteSessionCookie } from "$lib/db/authUtils.server.js";
+import { lucia } from "$lib/server/lucia";
 
 export const actions = {
   default: logout,
 };
 
-interface LogoutInterface {
-  locals: App.Locals;
-}
-
-async function logout({ locals }: LogoutInterface) {
-  const session = await locals.auth.validate();
-  if (!session) {
-    throw redirect(302, "/auth");
-  }
-  await auth.invalidateSession(session.sessionId); // Invalidate session
-  locals.auth.setSession(null); // Remove cookie
+async function logout({ cookies }: { cookies: Cookies }) {
+  deleteSessionCookie(lucia, cookies);
   throw redirect(302, "/auth"); // Redirect to auth page
 }
