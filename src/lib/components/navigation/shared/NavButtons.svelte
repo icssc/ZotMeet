7bb5818c6212d38e2203f96777dbe1e5af95589d
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2024-03-30 01:04:12 +0000

    feat: ✨ comment out settings tab

--- src/lib/components/navigation/shared/NavButtons.svelte
+++ src/lib/components/navigation/shared/NavButtons.svelte
@@ -3,7 +3,7 @@
 
   import AddBoxOutlineIcon from "~icons/material-symbols/add-box-outline";
   import CalendarSearchOutlineIcon from "~icons/mdi/calendar-search-outline";
-  import SettingsOutline from "~icons/mdi/settings-outline";
+  // import SettingsOutline from "~icons/mdi/settings-outline";
 </script>
 
 <div class="flex flex-col gap-y-2 border-t-2 px-6 pt-6 text-gray-600 sm:px-8 md:border-0 md:p-0">
@@ -15,7 +15,7 @@
     <CalendarSearchOutlineIcon class="h-7 w-7" />
   </NavButton>
 
-  <NavButton text="Settings" , href="/settings">
+  <!-- <NavButton text="Settings" , href="/settings">
     <SettingsOutline class="h-7 w-7" />
-  </NavButton>
+  </NavButton> -->
 </div>
