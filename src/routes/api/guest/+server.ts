ec8cb0c4a5bbadd4fccfd05e6005bdc90a145c22
HoussinSa <Houssin.saadeh@yahoo.com>
2024-03-31 10:28:21 -0700

    feat: ✨ set up base for Meeting flow

---
+++ src/routes/api/guest/+server.ts
@@ -0,0 +1,18 @@
+export async function POST({ request }: { request: Request }) {
+  const data = await request.json();
+
+  try {
+    // Update with the code neccesary to update the meetings table via drizzle
+
+    console.log(data, request);
+  } catch (error) {
+    console.error("error", error);
+    return new Response(JSON.stringify({ error: error }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+  return new Response(JSON.stringify({ message: "Success" }), { status: 201 });
+}
