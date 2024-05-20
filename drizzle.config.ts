a2cb804537752a7c9f4ef3a5d451983e27b480ef
Kevin Wu <kevinwu098@gmail.com>
2024-05-19 21:48:11 -0700

    chore: 🔧 update drizzle kit (#99)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,18 +4,17 @@ import type { Config } from "drizzle-kit";
 dotenv.config();
 
 const { DATABASE_URL } = process.env;
-  
+
 if (!DATABASE_URL) {
   throw new Error(
     "DATABASE_URL not found. Please ensure you have the DATABASE_URL variable defined inside of your environment configuration.",
   );
 }
-
 export default {
   schema: "./src/lib/db/schema.ts",
   out: "./src/lib/db/migrations",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: DATABASE_URL,
+    url: DATABASE_URL,
   },
 } satisfies Config;
