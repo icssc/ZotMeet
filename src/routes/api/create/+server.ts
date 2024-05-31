00ce4a466d228fd4ee21c3ba8d676679ffd25cf7
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2024-05-31 22:12:08 +0000

    feat: ✨ print to stderr

--- src/routes/api/create/+server.ts
+++ src/routes/api/create/+server.ts
@@ -41,7 +41,7 @@ export async function POST({ request }) {
     const meetingId = await insertMeeting(meeting, sortedDates);
     return json({ meetingId });
   } catch (err) {
-    console.log("Error creating meeting:", err);
+    console.error("Error creating meeting:", err, meeting, sortedDates);
     // TODO: This is unsafe
     throw error(500, `${err.stack}\nmeeting schema: ${JSON.stringify(meeting)}`);
   }
