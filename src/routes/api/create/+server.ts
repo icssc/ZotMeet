6374f04539955d50435f2ce506ee9ec95c4f9271
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2024-05-31 22:10:37 +0000

    fix: stringify JSON

--- src/routes/api/create/+server.ts
+++ src/routes/api/create/+server.ts
@@ -43,6 +43,6 @@ export async function POST({ request }) {
   } catch (err) {
     console.log("Error creating meeting:", err);
     // TODO: This is unsafe
-    throw error(500, `${err.stack}\nmeeting schema: ${meeting}`);
+    throw error(500, `${err.stack}\nmeeting schema: ${JSON.stringify(meeting)}`);
   }
 }
