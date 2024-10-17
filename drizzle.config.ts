f507f18b40884ecb6a951e578e399c2c220b3a0d
Sean Fong <seancfong@gmail.com>
2024-10-17 12:17:39 -0700

    feat: reformat drizzle config to updated version in documentation

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,21 +1,12 @@
-import * as dotenv from "dotenv";
-import type { Config } from "drizzle-kit";
+import "dotenv/config";
 
-dotenv.config();
+import { defineConfig } from "drizzle-kit";
 
-const { DATABASE_URL } = process.env;
-
-if (!DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL not found. Please ensure you have the DATABASE_URL variable defined inside of your environment configuration."
-  );
-}
-
-export default {
-  schema: "./src/db/schema.ts",
-  out: "./src/db/migrations",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: DATABASE_URL,
-  },
-} satisfies Config;
+export default defineConfig({
+    schema: "./src/db/schema.ts",
+    out: "./src/db/migrations",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+});
