9cafd7ae6fbbdc3733ef4f87686e4ef41182c953
Kevin Wu <kevinwu098@gmail.com>
2024-10-31 02:12:57 -0700

    feat: tabs

--- src/app/availability/[slug]/page.tsx
+++ src/app/availability/[slug]/page.tsx
@@ -1,8 +1,15 @@
 import { redirect } from "next/navigation";
 import { AvailabilityHeader } from "@/app/components/availability/availability-header";
 import { PersonalAvailability } from "@/app/components/availability/personal-availability";
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/app/components/custom/tabs";
 import { getExistingMeeting } from "@/lib/db/databaseUtils";
 import { getAvailability, getMeetingDates } from "@/lib/db/utils";
+import { cn } from "@/lib/utils";
 
 interface PageProps {
     params: {
@@ -27,15 +34,44 @@ export default async function Page({ params }: PageProps) {
     const availability = user ? await getAvailability(user, slug) : null;
 
     return (
-        <div>
+        <div className="space-y-2 px-4">
             <AvailabilityHeader meetingData={meetingData} />
 
-            <PersonalAvailability
-                columns={5}
-                meetingData={meetingData}
-                meetingDates={meetingDates}
-                availability={availability}
-            />
+            <Tabs
+                defaultValue="personal"
+                className={"space-y-6 px-6"}
+            >
+                <TabsList className="mx-6 space-x-0">
+                    <TabsTrigger
+                        value="personal"
+                        className={cn(
+                            "border-0 border-b-2 border-neutral-500 p-4 pb-0 text-lg duration-0",
+                            "data-[state=active]:border-orange-500"
+                        )}
+                    >
+                        Personal
+                    </TabsTrigger>
+                    <TabsTrigger
+                        value="password"
+                        className={cn(
+                            "border-0 border-b-2 border-neutral-500 p-4 pb-0 text-lg duration-0",
+                            "data-[state=active]:border-orange-500"
+                        )}
+                    >
+                        Group
+                    </TabsTrigger>
+                </TabsList>
+
+                <TabsContent value="personal">
+                    <PersonalAvailability
+                        columns={5}
+                        meetingData={meetingData}
+                        meetingDates={meetingDates}
+                        availability={availability}
+                    />
+                </TabsContent>
+                <TabsContent value="password">Group Availability</TabsContent>
+            </Tabs>
         </div>
     );
 }
