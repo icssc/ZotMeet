bd274e969644c82304cd990c2b4b0e9c28eeb3a7
Kevin Wu <kevinwu098@gmail.com>
2024-12-04 01:10:12 -0800

    feat: not found

--- src/app/availability/[slug]/page.tsx
+++ src/app/availability/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 import { AvailabilityHeader } from "@/components/availability/availability-header";
 import { PersonalAvailability } from "@/components/availability/personal-availability";
 import {
@@ -22,12 +22,18 @@ export default async function Page({ params }: PageProps) {
     const user = { id: "123" }; // TODO (#auth): replace with actual user from session
 
     if (!slug) {
-        redirect("/error");
+        notFound();
     }
 
-    const meetingData = await getExistingMeeting(slug);
+    const meetingData = await getExistingMeeting(slug).catch((e) => {
+        if (e instanceof Error) {
+            console.error(e);
+        }
+        notFound();
+    });
+
     if (!meetingData) {
-        redirect("/error");
+        notFound();
     }
 
     const meetingDates = await getMeetingDates(slug);
