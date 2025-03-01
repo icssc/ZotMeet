16852e44010757728705b99b66bf11d7c7fa545d
kylebtran <ucitrankyl@gmail.com>
2025-02-28 23:10:32 -0800

    feat: write meeting creation data into database

--- src/server/actions/meeting/create/action.ts
+++ src/server/actions/meeting/create/action.ts
@@ -1,31 +1,53 @@
 "use server";
 
 import { redirect } from "next/navigation";
+import { db } from "@/db";
+import { InsertMeeting, meetings } from "@/db/schema";
 import { getCurrentSession } from "@/lib/auth";
 import { CreateMeetingPostParams } from "@/lib/types/meetings";
 
-export async function createMeeting(newMeeting: CreateMeetingPostParams) {
+export async function createMeeting(meetingData: CreateMeetingPostParams) {
     try {
         const { title, description, fromTime, toTime, timezone, meetingDates } =
-            newMeeting;
+            meetingData;
 
-        // TODO: Re-write this according to the new database schema
-
-        // - Check if the user is logged in
         const { user } = await getCurrentSession();
 
-        // - If not, return an error (for now, guests are not allowed to create meetings)
         if (!user) {
             return { error: "You must be logged in to create a meeting." };
         }
-        // - If so, get the member id of the user for the meeting's host id
+        const hostId = user.memberId;
 
-        // - Check validity of the meeting dates and times (e.g. no duplicate dates, fromTime < toTime, etc.)
+        if (
+            fromTime >= toTime ||
+            meetingDates.length === 0 ||
+            new Set(meetingDates).size !== meetingDates.length ||
+            meetingDates.some(
+                (date) =>
+                    new Date(date).setHours(0, 0, 0, 0) <
+                    new Date().setHours(0, 0, 0, 0)
+            )
+        ) {
+            // Nondescript error message.
+            return { error: "Invalid meeting dates or times." };
+        }
 
-        // - Create a new row in the meetings table and get the id of the new meeting
+        const meeting: InsertMeeting = {
+            title,
+            description,
+            fromTime,
+            toTime,
+            timezone,
+            hostId,
+            dates: meetingDates.map((date: string) => new Date(date)),
+        };
 
+        const [newMeeting] = await db
+            .insert(meetings)
+            .values(meeting)
+            .returning({ id: meetings.id });
         // - On the server (300 resopnse code), redirect the user to the meeting page
-        redirect(`/availability/_`);
+        redirect(`/availability/${newMeeting.id}`);
     } catch (err) {
         const error = err as Error;
         console.error("Error creating meeting:", error.message);
