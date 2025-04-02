df340e52e616c9d70a25c54ffbc98e3454db7b2d
Kevin Wu <kevinwu098@gmail.com>
2025-04-02 14:11:12 -0700

    fix: 🐛 date selection bug

--- src/lib/zotdate.ts
+++ src/lib/zotdate.ts
@@ -57,11 +57,7 @@ export class ZotDate {
      * @return a number proportional to the amount of days elapsed since 0 AD, although not exact due to leap years
      */
     valueOf(): number {
-        return (
-            this.day.getDate() +
-            this.day.getMonth() * 31 +
-            this.day.getFullYear() * 366
-        );
+        return this.day.getTime();
     }
 
     /**
