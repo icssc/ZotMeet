b32eada6343817d09e340cf092e8954fcf08804a
aizilerts <annacpineapple2017@gmail.com>
2025-05-02 22:47:49 -0700

    fix: 🐛 small bug fixes

--- src/app/login/google/callback/route.tsx
+++ src/app/login/google/callback/route.tsx
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { setSessionTokenCookie } from "@/lib/auth/cookies";
 import { google } from "@/lib/auth/oauth";
 import { createSession, generateSessionToken } from "@/lib/auth/session";
-import { createUser } from "@/lib/auth/user";
+import { createGoogleUser } from "@/lib/auth/user";
 import { getUserFromGoogleId } from "@/server/data/user/queries";
 import { decodeIdToken } from "arctic";
 import type { OAuth2Tokens } from "arctic";
@@ -11,14 +11,10 @@ export async function GET(request: Request): Promise<Response> {
     const url = new URL(request.url);
     const code = url.searchParams.get("code");
     const state = url.searchParams.get("state");
+
     const cookieStore = await cookies();
     const storedState = cookieStore.get("google_oauth_state")?.value ?? null;
     const codeVerifier = cookieStore.get("google_code_verifier")?.value ?? null;
-
-    console.log("state", state);
-
-    console.log("storedState", storedState);
-    console.log("codeVerifier", codeVerifier);
 
     if (
         code === null ||
@@ -51,14 +47,12 @@ export async function GET(request: Request): Promise<Response> {
     const claims = decodeIdToken(tokens.idToken());
     const googleUserId = claims.sub;
     const username = claims.name;
-    console.log("googleUserId", googleUserId);
-    console.log("username", username);
 
     const existingUser = await getUserFromGoogleId(googleUserId);
-    console.log("existingUser", existingUser);
-    if (existingUser !== undefined) {
+
+    if (existingUser !== false) {
         const sessionToken = generateSessionToken();
-        const session = await createSession(sessionToken, existingUser.id);
+        const session = await createSession(sessionToken, googleUserId);
         await setSessionTokenCookie(sessionToken, session.expiresAt);
         return new Response(null, {
             status: 302,
@@ -67,8 +61,7 @@ export async function GET(request: Request): Promise<Response> {
             },
         });
     }
-
-    const user = await createUser(googleUserId, username, "oauth pass");
+    const user = await createGoogleUser(googleUserId, username, "oauth pass");
 
     const sessionToken = generateSessionToken();
     const session = await createSession(sessionToken, user.id);
