17558cba576e4aecd9ebaa49e745a1fbc7d18f04
Kevin Wu <kevinwu098@gmail.com>
2025-05-07 14:08:25 -0700

    fix: 🐛 password

--- src/lib/auth/password.ts
+++ src/lib/auth/password.ts
@@ -1,12 +1,11 @@
-import { hash, verify } from "argon2";
+import { hash, verify } from "@node-rs/argon2";
 
 export async function hashPassword(password: string): Promise<string> {
     return await hash(password, {
         memoryCost: 19456,
         timeCost: 2,
         parallelism: 1,
-        hashLength: 32,
-        type: 2,
+        outputLen: 32,
     });
 }
 
