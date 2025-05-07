6911de8f40d5e2165566f6304ca75fa4808ce85b
aizilerts <annacpineapple2017@gmail.com>
2025-05-06 21:37:55 -0700

    fix: 🐛 updated value to env

--- src/lib/auth/oauth.ts
+++ src/lib/auth/oauth.ts
@@ -3,5 +3,5 @@ import { Google } from "arctic";
 export const google = new Google(
     process.env.GOOGLE_OAUTH_CLIENT_ID || "default-client-id",
     process.env.GOOGLE_OAUTH_CLIENT_SECRET || "default-client-secret",
-    "http://localhost:3000/login/google/callback"
+    process.env.GOOGLE_OAUTH_REDIRECT_URI || "default-redirect-uri",
 );
