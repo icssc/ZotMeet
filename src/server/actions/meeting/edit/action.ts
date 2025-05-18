8613bb4e19401d8ee5a37d6b690be39b4f2357ab
aizilerts <annacpineapple2017@gmail.com>
2025-05-17 17:26:28 -0700

    fix: 🐛 changed editmeeting query so it awaits the db onces

--- src/server/actions/meeting/edit/action.ts
+++ src/server/actions/meeting/edit/action.ts
@@ -5,18 +5,18 @@ import { meetings } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { getCurrentSession } from "@/lib/auth";
 import { CreateMeetingPostParams } from "@/lib/types/meetings";
+import { SelectMeeting } from "@/db/schema";
 
-export async function editMeeting(id: string, meetingData: CreateMeetingPostParams) {
-    const meetingId = id;
 
-    const { title, description, fromTime, toTime, timezone, meetingDates } =
-        meetingData; //description and timezone could be edited in the future
-
+export async function editMeeting(meetingId: string, originalMeeting: SelectMeeting, meetingData: CreateMeetingPostParams) {
     const { user } = await getCurrentSession();
 
     if (!user) {
         return { error: "You must be logged in to edit a meeting." };
     }
+
+    let { title, description: _description, fromTime, toTime, timezone: _timezone, meetingDates } =
+        meetingData; //description and timezone could be edited in the future
 
     if (
         fromTime >= toTime ||
@@ -24,22 +24,15 @@ export async function editMeeting(id: string, meetingData: CreateMeetingPostPara
     ) {
         return { error: "Invalid meeting dates or times." };
     }
+    if (title === "") {
+        title = originalMeeting.title;
+    }
+    if (meetingDates.length === 0) {
+        meetingDates = originalMeeting.dates;
+    }
     await db.update(meetings)
-    .set({fromTime: fromTime, toTime: toTime})
+    .set({title: title, dates: meetingDates, fromTime: fromTime, toTime: toTime})
     .where(eq(meetings.id, meetingId));
-
-    if (title !== "") {
-        await db.update(meetings)
-        .set({title: title})
-        .where(eq(meetings.id, meetingId));
-    }
-    if (meetingDates.length > 0) {
-        await db.update(meetings)
-        .set({dates: meetingDates})
-        .where(eq(meetings.id, meetingId));
-    }
-
-    
 
     return {
         status: 200,
