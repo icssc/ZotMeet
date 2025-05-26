76941fdc7f63274e43df697c8e8efcc2ab87edd3
aizilerts <annacpineapple2017@gmail.com>
2025-05-26 14:29:32 -0700

    ci: 👷 formatting

--- src/server/actions/studyrooms/action.ts
+++ src/server/actions/studyrooms/action.ts
@@ -3,13 +3,14 @@ import type { paths } from "@/lib/types/anteater-api-types";
 type studyRoomsByFilters =
     paths["/v2/rest/studyRooms"]["get"]["parameters"]["query"];
 
-type studyRooms = paths["/v2/rest/studyRooms"]["get"]["responses"]["200"]["content"]["application/json"];
-
+type studyRooms =
+    paths["/v2/rest/studyRooms"]["get"]["responses"]["200"]["content"]["application/json"];
 
 export async function getStudyRoomProps(dates: string, times: string) {
-    
-    const query: studyRoomsByFilters = {dates, times};
-    const params = new URLSearchParams(query as Record<string, string>).toString();
+    const query: studyRoomsByFilters = { dates, times };
+    const params = new URLSearchParams(
+        query as Record<string, string>
+    ).toString();
     const apiUrl = `https://anteaterapi.com/v2/rest/studyRooms?${params}`;
 
     const res = await fetch(apiUrl);
@@ -23,5 +24,4 @@ export async function getStudyRoomProps(dates: string, times: string) {
     return {
         props: { data },
     };
-     
 }
