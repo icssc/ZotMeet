f4beab139f6479a9ef69749fc1ccaa351b16e55e
aizilerts <annacpineapple2017@gmail.com>
2025-10-06 16:28:07 -0700

    fix: 🐛 made conditional for adding archive column

--- src/db/migrations/0001_fantastic_puff_adder.sql
+++ src/db/migrations/0001_fantastic_puff_adder.sql
@@ -1,1 +1,5 @@
-ALTER TABLE "meetings" ADD COLUMN "archived" boolean DEFAULT false NOT NULL;
+DO $$ BEGIN
+  IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'meetings' AND column_name = 'archived') THEN
+    ALTER TABLE "meetings" ADD COLUMN "archived" boolean DEFAULT false NOT NULL;
+  END IF;
+END $$;
