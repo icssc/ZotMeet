423af60aff523688218d83084cf91c26b7dc0724
Kevin Wu <kevinwu098@gmail.com>
2025-11-16 14:50:00 -0800

    fix: 🐛 db migration

--- src/db/migrations/0001_fantastic_puff_adder.sql
+++ src/db/migrations/0001_fantastic_puff_adder.sql
@@ -1,1 +1,5 @@
-ALTER TABLE "meetings" ADD COLUMN "archived" boolean DEFAULT false NOT NULL;
+DO $$ BEGIN
+    ALTER TABLE "meetings" ADD COLUMN "archived" boolean DEFAULT false NOT NULL;
+EXCEPTION
+    WHEN duplicate_column THEN null;
+END $$;
