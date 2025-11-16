1762e4888d70ef95f5b53a6c102e4002dc340df1
Kevin Wu <kevinwu098@gmail.com>
2025-11-15 22:38:45 -0800

    fix: 🐛 drizzle migration

--- src/db/migrations/0002_lonely_mephistopheles.sql
+++ src/db/migrations/0002_lonely_mephistopheles.sql
@@ -1,2 +1,11 @@
-CREATE TYPE "public"."meeting_type" AS ENUM('dates', 'days');--> statement-breakpoint
-ALTER TABLE "meetings" ADD COLUMN "meeting_type" "meeting_type" DEFAULT 'dates' NOT NULL;
+DO $$ BEGIN
+    CREATE TYPE "public"."meeting_type" AS ENUM('dates', 'days');
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+    ALTER TABLE "meetings" ADD COLUMN "meeting_type" "meeting_type" DEFAULT 'dates' NOT NULL;
+EXCEPTION
+    WHEN duplicate_column THEN null;
+END $$;
