39f0c5cc7dc8e3175f0e156505b01af2cf10bef6
Ethan Chao <ethanchao2005@gmail.com>
2025-12-05 10:15:22 -0800

    feat: ✨ base 404 page implementation (#213)
    * feat: ✨ base 404 page implementation
    
    * fix: 🐛 typo in title
    
    * fix: 🐛 esLint apostrophe
    
    * feat: ✨ converted another apostrophe to eslint
    
    * fix: 🐛 changed availability route to use slug-based notFound handling
    
    * fix: 🐛 changed apostrophes to &apos;
    
    ---------
    
    Co-authored-by: Ethan Chao <ewchao1@uci.edu>

--- src/app/availability/[slug]/not-found.tsx
+++ src/app/availability/[slug]/not-found.tsx
@@ -6,10 +6,12 @@ export default function NotFound() {
         <div className="flex min-h-screen items-center justify-center">
             <div className="flex h-fit w-fit items-center justify-center gap-2 rounded-md border border-blue-400 bg-blue-100 p-16 shadow-sm">
                 <div className="flex flex-col items-center gap-3">
-                    <h2 className="text-2xl font-bold">Oops! Page Not Found</h2>
+                    <h2 className="text-2xl font-bold">
+                        Oops! Meeting Not Found
+                    </h2>
                     <p className="text-gray-600">
-                        The Page you&apos;re looking for doesn&apos;t exist or
-                        may have been moved
+                        The Meeting you&apos;re looking for doesn&apos;t exist
+                        or may have been moved
                     </p>
 
                     <div className="p-7">
