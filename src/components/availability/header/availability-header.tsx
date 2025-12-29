a13548e3c1c27d90dbf2cb3787d6267bda124e39
Nathan Nguyen <nathnguyen7631@gmail.com>
2025-12-28 17:02:29 -0800

    fix: 🐛 added backend check for meeting owner in archiveMeeting action

--- src/components/availability/header/availability-header.tsx
+++ src/components/availability/header/availability-header.tsx
@@ -178,7 +178,7 @@ export function AvailabilityHeader({
             />
 
             <DeleteModal
-                meetingId={meetingData.id}
+                meetingData={meetingData}
                 isOpen={isDeleteModalOpen}
                 handleOpenChange={setIsDeleteModalOpen}
             />
