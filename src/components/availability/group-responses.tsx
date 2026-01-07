22313fd114a44fdbeccd8a00a80fa651211d8c1f
Ethan Chao <ewchao1@uci.edu>
2026-01-07 09:57:33 -0800

    feat: ✨ shadcn checkbox stores selected users

--- src/components/availability/group-responses.tsx
+++ src/components/availability/group-responses.tsx
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useMemo, useState } from "react";
 import { useShallow } from "zustand/shallow";
 import { getTimestampFromBlockIndex } from "@/components/availability/group-availability";
 import type { Member } from "@/lib/types/availability";
+import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
 import { ZotDate } from "@/lib/zotdate";
 import { useAvailabilityViewStore } from "@/store/useAvailabilityViewStore";
@@ -21,22 +22,26 @@ export function GroupResponses({
 	members,
 	anchorNormalizedDate,
 }: GroupResponsesProps) {
-	const { availabilityView } = useAvailabilityViewStore();
-	const {
-		selectedZotDateIndex,
-		selectedBlockIndex,
-		isMobileDrawerOpen,
-		setIsMobileDrawerOpen,
-		setHoveredMember,
-	} = useGroupSelectionStore(
-		useShallow((state) => ({
-			selectedZotDateIndex: state.selectedZotDateIndex,
-			selectedBlockIndex: state.selectedBlockIndex,
-			isMobileDrawerOpen: state.isMobileDrawerOpen,
-			setIsMobileDrawerOpen: state.setIsMobileDrawerOpen,
-			setHoveredMember: state.setHoveredMember,
-		})),
-	);
+    const { availabilityView } = useAvailabilityViewStore();
+    const {
+        selectedZotDateIndex,
+        selectedBlockIndex,
+        isMobileDrawerOpen,
+        setIsMobileDrawerOpen,
+        setHoveredMember,
+        toggleSelectedMember,
+        selectedMembers,
+    } = useGroupSelectionStore(
+        useShallow((state) => ({
+            selectedZotDateIndex: state.selectedZotDateIndex,
+            selectedBlockIndex: state.selectedBlockIndex,
+            isMobileDrawerOpen: state.isMobileDrawerOpen,
+            setIsMobileDrawerOpen: state.setIsMobileDrawerOpen,
+            setHoveredMember: state.setHoveredMember,
+            toggleSelectedMember: state.toggleSelectedMember,
+            selectedMembers: state.selectedMembers,
+        }))
+    );
 
 	const [blockInfoString, setBlockInfoString] = useState(
 		"Select a cell to view",
@@ -49,16 +54,23 @@ export function GroupResponses({
 		[setHoveredMember],
 	);
 
-	const { availableMembers, notAvailableMembers } = useMemo(() => {
-		if (
-			selectedZotDateIndex === undefined ||
-			selectedBlockIndex === undefined
-		) {
-			return {
-				availableMembers: [],
-				notAvailableMembers: members,
-			};
-		}
+    const handleMemberSelect = useCallback(
+        (memberId: string) => {
+            toggleSelectedMember(memberId);
+        },
+        [toggleSelectedMember]
+    );
+
+    const { availableMembers, notAvailableMembers } = useMemo(() => {
+        if (
+            selectedZotDateIndex === undefined ||
+            selectedBlockIndex === undefined
+        ) {
+            return {
+                availableMembers: [],
+                notAvailableMembers: members,
+            };
+        }
 
 		const selectedDate = availabilityDates[selectedZotDateIndex];
 		const timestamp = getTimestampFromBlockIndex(
@@ -132,76 +144,102 @@ export function GroupResponses({
 				</p>
 			</div>
 
-			<div
-				className={cn(
-					"fixed right-0 bottom-0 h-96 w-full translate-y-full overflow-auto rounded-t-xl border-[1px] border-gray-400 bg-gray-100 bg-opacity-90 transition-transform duration-500 ease-in-out sm:right-0 sm:left-auto sm:w-96 lg:relative lg:top-0 lg:right-10 lg:h-auto lg:w-64 lg:translate-y-0 lg:self-stretch lg:rounded-l-xl lg:bg-opacity-50",
-					isMobileDrawerOpen && "translate-y-0",
-				)}
-			>
-				<div className="flex items-center justify-between px-8 py-4 lg:hidden">
-					<div>
-						<h3 className="font-medium font-montserrat">Responders</h3>
-						<p className="font-bold font-dm-sans text-slate-400 text-xs uppercase tracking-wide">
-							{blockInfoString}
-						</p>
-					</div>
-					<button
+            <div
+                className={cn(
+                    "fixed bottom-0 right-0 h-96 w-full translate-y-full overflow-auto rounded-t-xl border-[1px] border-gray-400 bg-gray-100 bg-opacity-90 transition-transform duration-500 ease-in-out sm:left-auto sm:right-0 sm:w-96 lg:relative lg:right-10 lg:top-0 lg:h-auto lg:w-64 lg:translate-y-0 lg:self-stretch lg:rounded-l-xl lg:bg-opacity-50",
+                    isMobileDrawerOpen && "translate-y-0"
+                )}
+            >
+                <div className="flex items-center justify-between px-8 py-4 lg:hidden">
+                    <div>
+                        <h3 className="font-montserrat font-medium">
+                            Responders
+                        </h3>
+                        <p className="font-dm-sans text-xs font-bold uppercase tracking-wide text-slate-400">
+                            {blockInfoString}
+                        </p>
+                    </div>
+                    <button
 						type="button"
-						className="rounded-lg border-[1px] border-slate-400 p-0.5 lg:hidden"
-						onClick={() => setIsMobileDrawerOpen(false)}
-					>
-						<XIcon className="text-lg text-slate-400" />
-					</button>
-				</div>
-				<div className="grid grid-cols-2 lg:flex lg:flex-col lg:gap-10 lg:py-4">
-					<div>
-						<div className="border-gray-300 border-b-[1px] px-8">
-							<span className="font-bold font-dm-sans text-slate-400 text-xs uppercase tracking-wide">
-								AVAILABLE ({availableMembers.length})
-							</span>
-						</div>
-						<ul className="h-64 overflow-auto py-2 pl-8">
-							{availableMembers.length > 0 ? (
-								availableMembers.map((member) => (
-									<li
-										key={member.memberId}
-										className="cursor-pointer text-gray-800 text-lg"
-										onMouseEnter={() => handleMemberHover(member.memberId)}
-										onMouseLeave={() => handleMemberHover(null)}
-									>
-										{member.displayName}
-									</li>
-								))
-							) : (
-								<li className="text-gray-400 text-sm italic">N/A</li>
-							)}
-						</ul>
-					</div>
-					<div>
-						<div className="border-gray-300 border-b-[1px] px-8">
-							<span className="font-bold font-dm-sans text-slate-400 text-xs uppercase tracking-wide">
-								NOT AVAILABLE ({notAvailableMembers.length})
-							</span>
-						</div>
-						<ul className="h-64 overflow-auto py-2 pl-8">
-							{notAvailableMembers.length > 0 ? (
-								notAvailableMembers.map((member) => (
-									<li
-										key={member.memberId}
-										className="cursor-pointer text-gray-400 text-lg"
-										onMouseEnter={() => handleMemberHover(member.memberId)}
-										onMouseLeave={() => handleMemberHover(null)}
-									>
-										{member.displayName}
-									</li>
-								))
-							) : (
-								<li className="text-gray-400 text-sm italic">N/A</li>
-							)}
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                        className="rounded-lg border-[1px] border-slate-400 p-0.5 lg:hidden"
+                        onClick={() => setIsMobileDrawerOpen(false)}
+                    >
+                        <XIcon className="text-lg text-slate-400" />
+                    </button>
+                </div>
+                <div className="grid grid-cols-2 lg:flex lg:flex-col lg:gap-10 lg:py-4">
+                    <div>
+                        <div className="border-b-[1px] border-gray-300 px-8">
+                            <span className="font-dm-sans text-xs font-bold uppercase tracking-wide text-slate-400">
+                                AVAILABLE ({availableMembers.length})
+                            </span>
+                        </div>
+                        <ul className="h-64 overflow-auto py-2 pl-8">
+                            {availableMembers.length > 0 ? (
+                                availableMembers.map((member) => (
+                                    <li
+                                        key={member.memberId}
+                                        className="cursor-pointer text-lg text-gray-800"
+                                        onMouseEnter={() =>
+                                            handleMemberHover(member.memberId)
+                                        }
+                                        onMouseLeave={() =>
+                                            handleMemberHover(null)
+                                        }
+                                    >
+                                        {member.displayName}
+                                    </li>
+                                ))
+                            ) : (
+                                <li className="text-sm italic text-gray-400">
+                                    N/A
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                    <div>
+                        <div className="border-b-[1px] border-gray-300 px-8">
+                            <span className="font-dm-sans text-xs font-bold uppercase tracking-wide text-slate-400">
+                                NOT AVAILABLE ({notAvailableMembers.length})
+                            </span>
+                        </div>
+                        <ul className="h-64 overflow-auto py-2 pl-8">
+                            {notAvailableMembers.length > 0 ? (
+                                notAvailableMembers.map((member) => (
+                                    <li
+                                        key={member.memberId}
+                                        className="cursor-pointer text-lg text-gray-400"
+                                        onMouseEnter={() =>
+                                            handleMemberHover(member.memberId)
+                                        }
+                                        onMouseLeave={() =>
+                                            handleMemberHover(null)
+                                        }
+                                    >
+                                        <div className="flex items-center gap-2">
+                                            <Checkbox
+                                                checked={selectedMembers.includes(
+                                                    member.memberId
+                                                )}
+                                                onCheckedChange={() =>
+                                                    handleMemberSelect(
+                                                        member.memberId
+                                                    )
+                                                }
+                                            />
+                                            {member.displayName}
+                                        </div>
+                                    </li>
+                                ))
+                            ) : (
+                                <li className="text-sm italic text-gray-400">
+                                    N/A
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
