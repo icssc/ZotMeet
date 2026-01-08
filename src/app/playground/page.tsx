4f5806c2b7efec315d3a9fa7521e2698457b3bb2
Kevin Wu <kevinwu098@gmail.com>
2026-01-08 02:17:14 -0800

    feat: ✨ button in playground

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@mui/material";
+
 export default function Page() {
     return (
         <div>
             <div>Playground</div>
+
+            <Button
+                variant="contained"
+                color="primary"
+            >
+                Click me
+            </Button>
         </div>
     );
 }
