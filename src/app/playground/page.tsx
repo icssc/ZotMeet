f080cc9f9efc120fb47822baf2dfface485f73da
Kevin Wu <kevinwu098@gmail.com>
2026-01-08 11:53:47 -0800

    feat: ✨ init mui (#244)
    * feat: ✨ init mui
    
    * feat: ✨ button in playground
    
    * fix: 🐛 deps
    
    * fix: 🐛 deps
    
    * fix: 🐛 deps
    
    * feat: ✨ deps
    
    * ci: 👷 sst non-verbose
    
    * ci: 👷 pnpm
    
    * fix: 🐛 fix

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { Button } from "@mui/material";
+
 export default function Page() {
 	return (
 		<div>
 			<div>Playground</div>
+
+			<Button variant="contained" color="primary">
+				Click me
+			</Button>
 		</div>
 	);
 }
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+	typography: {
+		fontFamily: "var(--font-dm-sans)",
+	},
+});
+
+export default theme;
