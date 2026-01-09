540dafb4057167dac53f19e5e6d18aa7985cf146
Arya Palanivel <aryapalanivel@gmail.com>
2026-01-08 17:02:12 -0800

    feat: ✨  Get All Groups that A User is in

--- src/server/data/groups/queries.ts
+++ src/server/data/groups/queries.ts
@@ -2,7 +2,11 @@ import "server-only";
 
 import { eq } from "drizzle-orm";
 import { db } from "@/db";
-import { groups, type SelectGroup } from "@/db/schema";
+import { 
+    groups, 
+    type SelectGroup,
+    usersInGroup 
+} from "@/db/schema";
 
 export async function getExistingGroup(groupId: string): Promise<SelectGroup> {
 	const group = await db.query.groups.findFirst({
@@ -13,6 +17,22 @@ export async function getExistingGroup(groupId: string): Promise<SelectGroup> {
 		throw new Error("Group not found");
 	}
 	return group;
+}
+
+export async function getGroupsByUserId(
+	userId: string,
+): Promise<SelectGroup[]> {
+	return await db
+		.select({
+			id: groups.id,
+			name: groups.name,
+			description: groups.description,
+			createdAt: groups.createdAt,
+			createdBy: groups.createdBy,
+		})
+		.from(groups)
+		.innerJoin(usersInGroup, eq(groups.id, usersInGroup.groupId))
+		.where(eq(usersInGroup.userId, userId));
 }
 //TODO: get all users in a group
 //TODO: get all groups that a user is in
