25f872bd23c36853bc85a926bcc635cc9a8f47cc
Ethan Chao <ethanchao2005@gmail.com>
2026-01-13 22:23:13 -0800

    feat: Specific Responders Availability (#234)
    Co-authored-by: Ethan Chao <ewchao1@uci.edu>
    Co-authored-by: Kevin Wu <kevinwu098@gmail.com>

--- src/store/useGroupSelectionStore.ts
+++ src/store/useGroupSelectionStore.ts
@@ -1,34 +1,58 @@
 import { create } from "zustand";
 
 interface GroupSelectionState {
-	selectedZotDateIndex: number | undefined;
-	selectedBlockIndex: number | undefined;
-	selectionIsLocked: boolean;
-	hoveredMember: string | null;
-	isMobileDrawerOpen: boolean;
-	setSelectedZotDateIndex: (index: number | undefined) => void;
-	setSelectedBlockIndex: (index: number | undefined) => void;
-	setSelectionIsLocked: (locked: boolean) => void;
-	setHoveredMember: (member: string | null) => void;
-	setIsMobileDrawerOpen: (open: boolean) => void;
-	resetSelection: () => void;
+    selectedZotDateIndex: number | undefined;
+    selectedBlockIndex: number | undefined;
+    selectionIsLocked: boolean;
+    hoveredMember: string | null;
+    isHoveringGrid: boolean;
+    selectedMembers: string[];
+    isMobileDrawerOpen: boolean;
+    setSelectedZotDateIndex: (index: number | undefined) => void;
+    setSelectedBlockIndex: (index: number | undefined) => void;
+    setSelectionIsLocked: (locked: boolean) => void;
+    setHoveredMember: (member: string | null) => void;
+    toggleHoverGrid: (val: boolean) => void;
+    toggleSelectedMember: (memberId: string) => void;
+    setSelectedMember: (members: string[]) => void;
+    setIsMobileDrawerOpen: (open: boolean) => void;
+    resetSelection: () => void;
 }
 
 export const useGroupSelectionStore = create<GroupSelectionState>((set) => ({
-	selectedZotDateIndex: undefined,
-	selectedBlockIndex: undefined,
-	selectionIsLocked: false,
-	hoveredMember: null,
-	isMobileDrawerOpen: false,
-	setSelectedZotDateIndex: (index) => set({ selectedZotDateIndex: index }),
-	setSelectedBlockIndex: (index) => set({ selectedBlockIndex: index }),
-	setSelectionIsLocked: (locked) => set({ selectionIsLocked: locked }),
-	setHoveredMember: (member) => set({ hoveredMember: member }),
-	setIsMobileDrawerOpen: (open) => set({ isMobileDrawerOpen: open }),
-	resetSelection: () =>
-		set({
-			selectedZotDateIndex: undefined,
-			selectedBlockIndex: undefined,
-			hoveredMember: null,
-		}),
+    selectedZotDateIndex: undefined,
+    selectedBlockIndex: undefined,
+    selectionIsLocked: false,
+    hoveredMember: null,
+    isHoveringGrid: false,
+    selectedMembers: [],
+    isMobileDrawerOpen: false,
+    setSelectedZotDateIndex: (index) => set({ selectedZotDateIndex: index }),
+    setSelectedBlockIndex: (index) => set({ selectedBlockIndex: index }),
+    setSelectionIsLocked: (locked) => set({ selectionIsLocked: locked }),
+    setHoveredMember: (member) => set({ hoveredMember: member }),
+    toggleHoverGrid: (val: boolean) => set({ isHoveringGrid: val }),
+    setSelectedMember: (members) => set({ selectedMembers: members }),
+    toggleSelectedMember: (memberId) =>
+        set((state) => {
+            const isSelected = state.selectedMembers.includes(memberId);
+            if (isSelected) {
+                return {
+                    selectedMembers: state.selectedMembers.filter(
+                        (id) => id !== memberId
+                    ),
+                };
+            } else {
+                return {
+                    selectedMembers: [...state.selectedMembers, memberId],
+                };
+            }
+        }),
+    setIsMobileDrawerOpen: (open) => set({ isMobileDrawerOpen: open }),
+    resetSelection: () =>
+        set({
+            selectedZotDateIndex: undefined,
+            selectedBlockIndex: undefined,
+            hoveredMember: null,
+        }),
 }));
