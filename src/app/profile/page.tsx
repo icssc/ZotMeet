0f06fa65d6e4e699ab0ccdc83479c4b73c053802
Arya Mhaiskar <mhaiskaa@uci.edu>
2026-03-10 23:30:21 -0700

    feat: ✨ added light and dark mode toggle to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { useState } from "react";
+
 export default function ProfilePage() {
+	const [mode, setMode] = useState("light");
+
 	return (
-		<div className="px-8 py-8">
+		<div className="space-y-6 px-8 py-8">
 			<h1 className="font-medium font-montserrat text-3xl">Profile</h1>
+
+			<div className="max-w-sm">
+				<p className="mb-2 font-medium">Preferred Display Mode</p>
+
+				<FormControl fullWidth>
+					<InputLabel id="theme-select-label">Mode</InputLabel>
+					<Select
+						labelId="theme-select-label"
+						value={mode}
+						label="Mode"
+						onChange={(e) => setMode(e.target.value)}
+					>
+						<MenuItem value="light">Light</MenuItem>
+						<MenuItem value="dark">Dark</MenuItem>
+					</Select>
+				</FormControl>
+			</div>
 		</div>
 	);
 }
