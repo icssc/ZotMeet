b81e0417a1194c28579764cfaad4e570c6599901
alixer-alex <152109823+alixer-alex@users.noreply.github.com>
2026-03-25 23:36:14 -0700

    fix: 🐛 combined both migrations together

--- src/db/migrations/0003_abnormal_typhoid_mary.sql
+++ src/db/migrations/0003_abnormal_typhoid_mary.sql
@@ -1,6 +1,13 @@
-ALTER TABLE "notifications" DROP CONSTRAINT IF EXISTS "notifications_user_id_users_member_id_fk";
---> statement-breakpoint
-ALTER TABLE "notifications" DROP CONSTRAINT IF EXISTS "notifications_created_by_users_member_id_fk";
+CREATE TABLE IF NOT EXISTS "notifications" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" uuid NOT NULL,
+	"type" text NOT NULL,
+	"read_at" timestamp with time zone,
+	"created_at" timestamp with time zone,
+	"created_by" uuid,
+	"title" text NOT NULL,
+	"message" text
+);
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "notifications" ADD CONSTRAINT "notifications_user_id_members_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."members"("id") ON DELETE cascade ON UPDATE no action;
