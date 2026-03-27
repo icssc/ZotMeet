dcf51e05e32cb9b510e41ecac2a00c3ddeaf797e
aizilerts <annacpineapple2017@gmail.com>
2026-03-27 12:07:34 -0700

    fix: 🐛 updated from looking at cubic

--- src/components/creation/fields/meeting-location-field.tsx
+++ src/components/creation/fields/meeting-location-field.tsx
@@ -34,7 +34,6 @@ export function MeetingLocationField({
 				placeholder="Location"
 				value={meetingLocation}
 				onChange={handleLocationChange}
-				autoFocus
 			/>
 			<FormControlLabel
 				control={
--- src/components/creation/fields/meeting-time-field.tsx
+++ src/components/creation/fields/meeting-time-field.tsx
@@ -92,8 +92,9 @@ export const MeetingTimeField = ({
 					size="medium"
 					placeholder="9:00 AM"
 					value={startTime}
-					onChange={(e) => setStartTime(e.target.value as HourMinuteString)}
-					autoFocus
+					onChange={(e) =>
+						setStartTime(`${e.target.value}:00` as HourMinuteString)
+					}
 				/>
 
 				<TextField
@@ -104,8 +105,9 @@ export const MeetingTimeField = ({
 					size="medium"
 					placeholder="5:00 PM"
 					value={endTime}
-					onChange={(e) => setEndTime(e.target.value as HourMinuteString)}
-					autoFocus
+					onChange={(e) =>
+						setEndTime(`${e.target.value}:00` as HourMinuteString)
+					}
 				/>
 				<div className="flex gap-2"></div>
 			</div>
