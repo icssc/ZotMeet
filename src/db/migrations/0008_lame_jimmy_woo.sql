fcc64779e9bfd0d53e04cbbed871b99e89728cf8
Arya Mhaiskar <mhaiskaa@uci.edu>
2026-04-03 12:11:58 -0700

    fix: 🐛 attempting to fix migration error

--- src/db/migrations/0008_lame_jimmy_woo.sql
+++ src/db/migrations/0008_lame_jimmy_woo.sql
@@ -1,2 +1,3 @@
+DROP TYPE IF EXISTS "public"."theme_mode";--> statement-breakpoint
 CREATE TYPE "public"."theme_mode" AS ENUM('light', 'dark', 'system');--> statement-breakpoint
 ALTER TABLE "users" ADD COLUMN "theme_mode" "theme_mode" DEFAULT 'light' NOT NULL;
