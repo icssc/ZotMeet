d123268445fa66c2fbfb36b894683b52dcca8c60
Arya Mhaiskar <mhaiskaa@uci.edu>
2026-04-03 11:54:59 -0700

    feat: ✨ saving user theme pref to db and loading it upon mount

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { getUserThemeMode, saveThemePreference } from "@actions/user/action";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { getTheme } from "@/theme";
@@ -28,6 +29,22 @@ export default function AppThemeProvider({
 	const [mode, setMode] = useState<ThemeMode>("light");
 	// resolving what "system" means for the current OS
 	const [resolvedMode, setResolvedMode] = useState<"light" | "dark">("light");
+	const [initialThemeLoaded, setInitialThemeLoaded] = useState(false);
+
+	// on mount, fetch user's theme preference from DB
+	useEffect(() => {
+		async function fetchThemePreference() {
+			try {
+				const themeMode = await getUserThemeMode();
+				setMode(themeMode);
+				setInitialThemeLoaded(true);
+			} catch (err) {
+				console.error("Failed to fetch theme preference", err);
+			}
+		}
+
+		fetchThemePreference();
+	}, []);
 
 	// Update resolvedMode whenever mode or system preference changes
 	useEffect(() => {
@@ -43,7 +60,26 @@ export default function AppThemeProvider({
 		}
 	}, [mode]);
 
+	// saving user preference to DB if it changed
+	useEffect(() => {
+		async function persistMode() {
+			try {
+				await saveThemePreference(mode);
+			} catch (err) {
+				console.error("Failed to save theme preference", err);
+			}
+		}
+		if (initialThemeLoaded) {
+			persistMode();
+		}
+	}, [mode, initialThemeLoaded]);
+
 	const theme = useMemo(() => getTheme(resolvedMode), [resolvedMode]);
+
+	if (!initialThemeLoaded) {
+		// Don't render children until we know the user's theme preference
+		return null;
+	}
 
 	return (
 		<ThemeModeContext.Provider value={{ mode, setMode }}>
