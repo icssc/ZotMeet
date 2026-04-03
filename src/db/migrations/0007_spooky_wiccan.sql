8474d778968a0dbacf4d6f8ecac3b61e94fb6ee0
Arya Mhaiskar <mhaiskaa@uci.edu>
2026-04-03 10:16:20 -0700

    fix: 🐛 applied hint from staging failure

--- src/db/migrations/0007_spooky_wiccan.sql
+++ src/db/migrations/0007_spooky_wiccan.sql
@@ -1,2 +1,6 @@
-CREATE TYPE "public"."theme_mode" AS ENUM('light', 'dark', 'system');--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "theme_mode" SET DATA TYPE theme_mode;
+CREATE TYPE "public"."theme_mode" AS ENUM('light', 'dark', 'system');
+
+ALTER TABLE "users"
+ALTER COLUMN "theme_mode"
+SET DATA TYPE theme_mode
+USING "theme_mode"::theme_mode;
