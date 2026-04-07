67827f69f521decb0e07d032de5befa7cd430392
alixer-alex <152109823+alixer-alex@users.noreply.github.com>
2026-04-07 12:49:52 -0700

    feat: ✨ notification feedback
    displays a small alert when the notification was sent successfully.

--- src/components/availability/notification-nudge.tsx
+++ src/components/availability/notification-nudge.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { sendNotificationsToUsers } from "@actions/user/action";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import Alert from "@mui/material/Alert";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import type { SelectMeeting } from "@/db/schema";
 import type { MemberMeetingAvailability } from "@/lib/types/availability";
@@ -17,21 +19,60 @@ export function NotificationNudge({
 			notificationids.add(availability.memberId);
 		}
 	}
+
+	const [notificationSuccess, setNotificationSuccess] = useState<
+		boolean | null
+	>(null);
 	const sendNotification = async () => {
-		await sendNotificationsToUsers(
-			[...notificationids],
-			meetingData.title,
-			"You have been notified about the meeting " + meetingData.title,
-			meetingData.meetingType,
-			window.location.href,
-		);
+		try {
+			await sendNotificationsToUsers(
+				[...notificationids],
+				meetingData.title,
+				"You have been notified about the meeting " + meetingData.title,
+				meetingData.meetingType,
+				window.location.href,
+			);
+			setNotificationSuccess(true);
+		} catch (error) {
+			console.error("Error sending notifications:", error);
+			setNotificationSuccess(false);
+		}
 	};
-	return (
-		<div className="pt-2">
-			<Button onClick={sendNotification}>
-				<NotificationsOutlinedIcon color="secondary" />
-				Notify Everyone
-			</Button>
-		</div>
-	);
+	if (notificationSuccess != null) {
+		if (notificationSuccess) {
+			return (
+				<div className="pt-2">
+					<Button onClick={sendNotification}>
+						<NotificationsOutlinedIcon color="secondary" />
+						Notify Everyone
+					</Button>
+					<Alert
+						severity="success"
+						onClose={() => setNotificationSuccess(null)}
+					>
+						Notifications sent successfully!
+					</Alert>
+				</div>
+			);
+		} else {
+			<div className="pt-2">
+				<Button onClick={sendNotification}>
+					<NotificationsOutlinedIcon color="secondary" />
+					Notify Everyone
+				</Button>
+				<Alert severity="error" onClose={() => setNotificationSuccess(null)}>
+					Failed to send notifications.
+				</Alert>
+			</div>;
+		}
+	} else {
+		return (
+			<div className="pt-2">
+				<Button onClick={sendNotification}>
+					<NotificationsOutlinedIcon color="secondary" />
+					Notify Everyone
+				</Button>
+			</div>
+		);
+	}
 }
