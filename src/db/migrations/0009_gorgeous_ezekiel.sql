1865bebd1bc8bd52dda6aef6095ecc4bbf87f96a
realethantran <etran8@gmail.com>
2026-04-13 23:55:47 -0700

    feat: ✨ switch from major to UCI specific school uses MUI autocomplete component

--- src/db/migrations/0009_gorgeous_ezekiel.sql
+++ src/db/migrations/0009_gorgeous_ezekiel.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "members" ADD COLUMN "username" text;--> statement-breakpoint
 ALTER TABLE "members" ADD COLUMN "year" text;--> statement-breakpoint
-ALTER TABLE "members" ADD COLUMN "major" text;--> statement-breakpoint
+ALTER TABLE "members" ADD COLUMN "school" text;--> statement-breakpoint
 ALTER TABLE "members" ADD COLUMN "profile_picture" text;--> statement-breakpoint
 ALTER TABLE "members" ADD CONSTRAINT "members_username_unique" UNIQUE("username");
