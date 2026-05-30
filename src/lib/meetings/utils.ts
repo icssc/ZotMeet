f036cd11939b2c665e1c83a399b45b0a6141ddee
ethancha0 <ethanchao2005@gmail.com>
2026-05-30 15:12:46 -0700

    fix: 🐛 version nibble

--- src/lib/meetings/utils.ts
+++ src/lib/meetings/utils.ts
@@ -3,7 +3,7 @@ import type { SelectMeeting, SelectScheduledMeeting } from "@/db/schema";
 const UPCOMING_WINDOW_MS = 3 * 24 * 60 * 60 * 1000;
 
 const UUID_RE =
-	/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+	/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 function isUuid(value: string): boolean {
 	return UUID_RE.test(value);
