02d7ab55963a6747e2dfb00ffd016aaf176c8d91
Celine Nguyen <celinenguyenn13@gmail.com>
2026-09-02 21:23:33 -0700

    feat: ✨ testing

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+	return (
+		<footer className="w-full bg-gray-800 py-4 text-white">
+			<Container maxWidth="lg">
+				<p>Footer</p>
+			</Container>
+		</footer>
+	);
+}
+
+export default Footer;
