2a859ebaffc6f8328414159a3d23a973944f733b
ethancha0 <ethanchao2005@gmail.com>
2026-09-25 10:07:41 -0700

    feat: ✨ file structure

--- src/app/(creation)/page.tsx
+++ src/app/(creation)/page.tsx
@@ -1,7 +1,8 @@
 import { Creation } from "@/components/creation/creation";
+import { Landing } from "@/components/landing/landing";
 import { getCurrentSession } from "@/lib/auth";
 
 export default async function Page() {
 	const { user } = await getCurrentSession();
-	return <Creation user={user} />;
+	return user ? <Creation user={user} /> : <Landing />;
 }
